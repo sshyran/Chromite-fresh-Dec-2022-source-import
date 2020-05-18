a8073be3cd5e4379f4c6cceb91bc761e812cb4a3
Mike Frysinger <vapier@chromium.org>
2020-05-18 14:35:41 -0400

    lint: fix interp used with pylint-2 wrapper
    Forgot to update the wrapper to use vpython3 after switching to our
    own copy.
    
    BUG=chromium:1006448
    TEST=`cros lint --py3` invokes our pylint-2 version through vpython
    
    Change-Id: I59cbaca5d05bc93f82a2f2fd2ad1c74fca03a600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2207674
    Reviewed-by: David Burger <dburger@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- cli/cros/cros_lint.py
+++ cli/cros/cros_lint.py
@@ -169,7 +169,7 @@ def _PylintFile(path, output_format, debug, interp):
   if interp != 'python3':
     vpython = os.path.join(constants.DEPOT_TOOLS_DIR, 'vpython')
   else:
-    vpython = interp
+    vpython = os.path.join(constants.DEPOT_TOOLS_DIR, 'vpython3')
     pylint = os.path.join(constants.CHROMITE_DIR, 'cli', 'cros', 'pylint-2')
   pylintrc = _GetPylintrc(path)
   cmd = [vpython, pylint, '--rcfile=%s' % pylintrc]
