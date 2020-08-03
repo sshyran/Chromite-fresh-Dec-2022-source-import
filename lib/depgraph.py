f4e3205d7df3ea1c884b1a4b10c1f23e81b64d3a
KevinRuvalcaba <kevinruvalcaba@google.com>
2020-08-03 21:02:27 +0000

    Adding root calculation to DepVisualizer class
    Calculating root nodes based on the number of reverse
    dependencies a node has; a root node must have zero.
    
    BUG=chromium:1103845
    TEST=None
    
    Change-Id: I4cf1c3d161f21fbb890ff2ff0849db6b1bd276de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2333810
    Reviewed-by: Alex Klein <saklein@chromium.org>
    Tested-by: Kevin Ruvalcaba <kevinruvalcaba@google.com>
    Commit-Queue: Kevin Ruvalcaba <kevinruvalcaba@google.com>

--- lib/depgraph.py
+++ lib/depgraph.py
@@ -906,6 +906,16 @@ class DepVisualizer(object):
       pkg_node.AddDependency(dep_node)
       dep_node.AddRvsDependency(pkg_node)
 
+def CalculateRoots(self):
+  """Determine which nodes are roots and return them in a List.
+
+  In this context a root node is one that no other depends on, in
+  other words those nodes with no reverse dependencies.
+
+  Returns:
+    A List of PackageNodes.
+  """
+  return [x for x in self.pkg_dict.values() if not x.rvs_dependencies]
 
 def PrintDepsMap(deps_map):
   """Print dependency graph, for each package list it's prerequisites."""
