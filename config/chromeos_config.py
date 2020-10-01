1f71c27547a01dbfa1521d02842acd2c4ff9cf55
Fritz Koenig <frkoenig@chromium.org>
2020-10-01 12:42:33 -0700

    trogdor: enable hw tests
    DUTs are in the lab
    
    BUG=b:152055929
    BUG=b:169275884
    TEST=none
    
    Change-Id: Iea0000321af0e399389a022ce4b37c5ec2bdaf71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2443582
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>
    Tested-by: Bob Moragues <moragues@chromium.org>
    Commit-Queue: Bob Moragues <moragues@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2530,10 +2530,6 @@ def ApplyCustomOverrides(site_config):
 
       'trogdor-release': {
           'sign_types': ['recovery', 'factory'],
-          # Trogdor has no DUTs in the lab. (b/152055929)
-          'hw_tests': [],
-          'hw_tests_override': [],
-          'hw_tests_disabled_bug': 'b/152055929',
       },
 
       'trogdor64-release': {
