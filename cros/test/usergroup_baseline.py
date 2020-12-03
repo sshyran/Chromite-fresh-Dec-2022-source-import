ae37051c41ee85d0579d9bb31dfe7894fbcda297
Chujia Liu <nerv@google.com>
2020-12-02 17:35:54 -0800

    usergroup_baseline: Add ap-process-manager to gwifi
    BUG=b:173813425
    TEST=test_image --board=kevin $IMG_PATH UserGroupTest
    
    Cq-Depend: chrome-internal:3442497
    Change-Id: I63238c4bf7c574dcf7cd0264c2c1fa77ead3f164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2570631
    Reviewed-by: Tao Jin <jintao@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Chujia Liu <nerv@google.com>
    Commit-Queue: Chujia Liu <nerv@google.com>

--- cros/test/usergroup_baseline.py
+++ cros/test/usergroup_baseline.py
@@ -236,10 +236,10 @@ GROUP_BASELINE_JETSTREAM = dict((e.group, e) for e in (
                       'ap-lb-update-manager', 'ap-monitor', 'ap-monlog',
                       'ap-net-acc-manager', 'ap-net-controller',
                       'ap-net-monitor', 'ap-pal', 'ap-pcap-manager', 'ap-pfd',
-                      'ap-pipe-reader', 'ap-qos-monitor', 'ap-rodizio',
-                      'ap-taxonomy', 'ap-update-manager', 'ap-vorlon-client',
-                      'ap-wifi-diagnostics', 'ap-wifi-manager',
-                      'ap-wireless-optimizer', 'gdisp'}),
+                      'ap-pipe-reader', 'ap-process-manager', 'ap-qos-monitor',
+                      'ap-rodizio', 'ap-taxonomy', 'ap-update-manager',
+                      'ap-vorlon-client', 'ap-wifi-diagnostics',
+                      'ap-wifi-manager', 'ap-wireless-optimizer', 'gdisp'}),
 ))
 
 # rialtod:!:400:rialto
