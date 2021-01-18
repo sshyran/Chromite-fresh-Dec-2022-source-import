c895583f931412f19fa1222a487ea7cc14e7986a
Kuo Jen Wei <inker@google.com>
2021-01-18 14:31:03 +0800

    chromite: add hotline group to expected group of base profile
    BUG=chromium:1167282
    TEST=usergroup_baseline.py
    TEST=sudo test_image --board buddy /mnt/host/source/src/build/
    images/buddy/R90-13734.0.2021_01_18_1313-a1/chromiumos_image.bin
    
    Change-Id: I52887394259ccea47caa91e18652645e6d336315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2632560
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Inker Kuo <inker@chromium.org>
    Tested-by: Inker Kuo <inker@chromium.org>
    Auto-Submit: Inker Kuo <inker@chromium.org>

--- cros/test/usergroup_baseline.py
+++ cros/test/usergroup_baseline.py
@@ -201,6 +201,7 @@ GROUP_BASELINE = dict((e.group, e) for e in (
                                                           'crosvm'}),
     GroupEntry(group='traced-consumer', gid=20164, users={'traced',
                                                           'chronos'}),
+    GroupEntry(group='hotline', gid=20157, users={'hotline', 'hotlog'}),
 ))
 
 GROUP_BASELINE_LAKITU = dict((e.group, e) for e in (
@@ -256,10 +257,6 @@ GROUP_BASELINE_TERMINA = dict((e.group, e) for e in (
     GroupEntry(group='lxd', gid=298, users={'lxd', 'chronos'}),
 ))
 
-GROUP_BASELINE_CFM = dict((e.group, e) for e in (
-    GroupEntry(group='hotline', gid=20157, users={'hotline', 'hotlog'}),
-))
-
 GROUP_BOARD_BASELINES = {
     'lakitu': GROUP_BASELINE_LAKITU,
     'lakitu-gpu': GROUP_BASELINE_LAKITU,
@@ -274,9 +271,4 @@ GROUP_BOARD_BASELINES = {
     'veyron_rialto': GROUP_BASELINE_RIALTO,
     'tael': GROUP_BASELINE_TERMINA,
     'tatl': GROUP_BASELINE_TERMINA,
-    'fizz': GROUP_BASELINE_CFM,
-    'fizz-cfm': GROUP_BASELINE_CFM,
-    'kalista': GROUP_BASELINE_CFM,
-    'kalista-cfm': GROUP_BASELINE_CFM,
-    'endeavour': GROUP_BASELINE_CFM,
 }
