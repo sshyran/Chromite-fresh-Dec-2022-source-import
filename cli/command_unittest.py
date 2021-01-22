293e4ca9e218d37247d669b4f0d037411ad62250
Mike Frysinger <vapier@chromium.org>
2021-01-22 07:43:15 -0500

    command: convert to python3 only
    All users of this module are python3 only already.
    
    BUG=chromium:997354
    TEST=`./run_tests` passes
    
    Change-Id: I68a77c49b0d470fd27d6e6b8e4ff15ae205a1898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2646227
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Alex Klein <saklein@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>

--- cli/command_unittest.py
+++ cli/command_unittest.py
@@ -10,11 +10,15 @@ from __future__ import print_function
 import argparse
 import importlib
 import os
+import sys
 
 from chromite.cli import command
 from chromite.lib import commandline
 from chromite.lib import cros_test_lib
 from chromite.lib import partial_mock
+
+
+assert sys.version_info >= (3, 6), 'This module requires Python 3.6+'
 
 
 # pylint:disable=protected-access
