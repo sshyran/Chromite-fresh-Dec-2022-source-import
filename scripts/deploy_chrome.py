b44d8e788bb4833ff8f39ab4e401f7db92feffc9
Ben Pastene <bpastene@chromium.org>
2021-02-19 13:56:43 -0800

    deploy_chrome: Remove '--to' and '--port' args.
    These two args were deperacted a while ago in favor of '--device',
    but their last use was finally removed in crrev.com/c/2386300.
    So we should be good to fully remove the old args
    
    BUG=chromium:1115724
    TEST=unittest
    TEST=deploy_chrome'ed locally
    
    Change-Id: I827d8fcfe998f1103f5fcbc444d200c19fc2d939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2708049
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Tested-by: Ben Pastene <bpastene@chromium.org>

--- scripts/deploy_chrome.py
+++ scripts/deploy_chrome.py
@@ -137,12 +137,8 @@ class DeployChrome(object):
     self.options = options
     self.staging_dir = staging_dir
     if not self.options.staging_only:
-      if options.device:
-        hostname = options.device.hostname
-        port = options.device.port
-      else:
-        hostname = options.to
-        port = options.port
+      hostname = options.device.hostname
+      port = options.device.port
       self.device = remote.ChromiumOSDevice(hostname, port=port,
                                             ping=options.ping,
                                             private_key=options.private_key,
@@ -200,7 +196,7 @@ class DeployChrome(object):
       if not cros_build_lib.BooleanPrompt('Remove rootfs verification?', False):
         return False
 
-    logging.info('Removing rootfs verification from %s', self.options.to)
+    logging.info('Removing rootfs verification from %s', self.options.device)
     # Running in VM's cause make_dev_ssd's firmware sanity checks to fail.
     # Use --force to bypass the checks.
     cmd = ('/usr/share/vboot/bin/make_dev_ssd.sh --partitions %d '
@@ -595,12 +591,6 @@ def _CreateParser():
                       default=True,
                       help="Don't strip binaries during deployment.  Warning: "
                            'the resulting binaries will be very large!')
-  parser.add_argument('-p', '--port', type=int, default=remote.DEFAULT_SSH_PORT,
-                      help='This arg is deprecated. Please use --device '
-                           'instead.')
-  parser.add_argument('-t', '--to', deprecated='Use --device instead',
-                      help='This arg is deprecated. Please use --device '
-                           'instead.')
   parser.add_argument(
       '-d', '--device',
       type=commandline.DeviceParser(commandline.DEVICE_SCHEME_SSH),
@@ -724,7 +714,7 @@ def _ParseCommandLine(argv):
       parser.error('--board is required')
   if options.gs_path and options.local_pkg_path:
     parser.error('Cannot specify both --gs-path and --local-pkg-path')
-  if not (options.staging_only or options.to or options.device):
+  if not (options.staging_only or options.device):
     parser.error('Need to specify --device')
   if options.staging_flags and not options.build_dir:
     parser.error('--staging-flags require --build-dir to be set.')
@@ -743,14 +733,6 @@ def _ParseCommandLine(argv):
 
   if options.mount and not options.mount_dir:
     options.mount_dir = _CHROME_DIR
-
-  if options.to:
-    if options.device:
-      parser.error('--to and --device are mutually exclusive.')
-    else:
-      logging.warning(
-          "The args '--to' & '--port' are deprecated. Please use '--device' "
-          'instead.')
 
   return options
 
