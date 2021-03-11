4d6603481d0b92af05673f590ef3dcc539d6483c
Allen Webb <allenwebb@google.com>
2021-03-11 09:23:41 -0600

    usergroup_baseline: Sort by uid/gid and add comments for ranges.
    BUG=None
    TEST=CQ passes
    
    Change-Id: Iac6845fa991fe0f3283e3f47e2bb1d84e01fa61b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2752595
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Allen Webb <allenwebb@google.com>
    Tested-by: Allen Webb <allenwebb@google.com>

--- cros/test/usergroup_baseline.py
+++ cros/test/usergroup_baseline.py
@@ -28,6 +28,7 @@ _VALID_LOGIN_SHELLS = set((
 ))
 
 USER_BASELINE = dict((e.user, e) for e in (
+    # These are sorted by uid.
     UserEntry(user='root', encpasswd='x', uid=0, gid=0, home='/root',
               shell=_VALID_LOGIN_SHELLS),
     UserEntry(user='bin', uid=1, gid=1, home='/bin'),
@@ -36,32 +37,41 @@ USER_BASELINE = dict((e.user, e) for e in (
     UserEntry(user='lp', uid=4, gid=7, home='/var/spool/lpd'),
     UserEntry(user='news', uid=9, gid=13, home='/var/spool/news'),
     UserEntry(user='uucp', uid=10, gid=14, home='/var/spool/uucp'),
-    UserEntry(user='portage', uid=250, gid=250, home='/var/tmp/portage'),
-    UserEntry(user='nobody', uid=65534, gid=65534),
-    UserEntry(user='chronos', encpasswd='x', uid=1000, gid=1000,
-              home='/home/chronos/user', shell=_VALID_LOGIN_SHELLS),
-    UserEntry(user='chronos-access', uid=1001, gid=1001),
+
+    # CrOS system daemon UIDs.
     UserEntry(user='sshd', uid=204, gid=204),
     UserEntry(user='tss', uid=207, gid=207, home='/var/lib/tpm'),
     UserEntry(user='dhcp', uid=224, gid=224,
               home={'/var/lib/dhcp', '/dev/null'}),
     UserEntry(user='goofy', encpasswd='x', uid=248, gid=248,
               home='/home/goofy', shell='/bin/bash'),
-    UserEntry(user='android-root', uid=655360, gid=655360),
+    UserEntry(user='portage', uid=250, gid=250, home='/var/tmp/portage'),
+
+    UserEntry(user='chronos', encpasswd='x', uid=1000, gid=1000,
+              home='/home/chronos/user', shell=_VALID_LOGIN_SHELLS),
+    UserEntry(user='chronos-access', uid=1001, gid=1001),
+
     UserEntry(user='user-containers', uid=10000, gid=10000),
+
+    UserEntry(user='nobody', uid=65534, gid=65534),
+
+    UserEntry(user='android-root', uid=655360, gid=655360),
 ))
 
 USER_BASELINE_LAKITU = dict((e.user, e) for e in (
+    # These are sorted by uid.
     UserEntry(user='systemd-timesync', uid=271, gid=271),
     UserEntry(user='systemd-network', uid=274, gid=274),
     UserEntry(user='systemd-resolve', uid=275, gid=275),
 ))
 
 USER_BASELINE_JETSTREAM = dict((e.user, e) for e in (
+    # These are sorted by uid.
     UserEntry(user='ap-monitor', uid=1103, gid=1103),
 ))
 
 USER_BASELINE_TERMINA = dict((e.user, e) for e in (
+    # These are sorted by uid.
     UserEntry(user='lxd', uid=298, gid=298),
 ))
 
@@ -77,6 +87,7 @@ USER_BOARD_BASELINES = {
 }
 
 GROUP_BASELINE = dict((e.group, e) for e in (
+    # These are sorted by gid.
     GroupEntry(group='root', gid=0, users={'root'}),
     GroupEntry(group='bin', gid=1, users={'root', 'bin', 'daemon'}),
     GroupEntry(group='daemon', gid=2, users={'root', 'bin', 'daemon'}),
@@ -105,18 +116,8 @@ GROUP_BASELINE = dict((e.group, e) for e in (
     GroupEntry(group='cdrw', gid=80, users={'cros-disks'}),
     GroupEntry(group='usb', gid=85, users={'mtp', 'brltty', 'dlm', 'modem'}),
     GroupEntry(group='users', gid=100),
-    GroupEntry(group='portage', gid=250, users={'portage'}),
-    GroupEntry(group='utmp', gid=406),
-    GroupEntry(group='crash-access', gid=419,
-               users={'crash'}),
-    GroupEntry(group='crash-user-access', gid=420,
-               users={'crash', 'chronos', 'vm_cicerone'}),
-    GroupEntry(group='nogroup', gid=65533),
-    GroupEntry(group='nobody', gid=65534),
-    GroupEntry(group='chronos', gid=1000),
-    GroupEntry(group='chronos-access', gid=1001,
-               users={'root', 'ipsec', 'chronos',
-                      'cros-disks', 'imageloaderd', 'crash', 'dlp'}),
+
+    # CrOS system daemon GIDs.
     GroupEntry(group='tss', gid=207, users={'root', 'attestation',
                                             'bootlockboxd', 'chaps',
                                             'oobe_config_restore',
@@ -124,17 +125,15 @@ GROUP_BASELINE = dict((e.group, e) for e in (
                                             'tpm_manager', 'trunks'}),
     GroupEntry(group='pkcs11', gid=208, users={'root', 'ipsec', 'chronos',
                                                'chaps', 'wpa', 'attestation'}),
+    GroupEntry(group='ipsec', gid=212, users={'shill'}),
     GroupEntry(group='wpa', gid=219, users={'root'}),
     GroupEntry(group='input', gid=222, users={'cras', 'power', 'chronos'}),
-    GroupEntry(group='cras', gid=600, users={'chronos', 'crosvm', 'power',
-                                             'rtanalytics', 'sound_card_init'}),
-    GroupEntry(group='wayland', gid=601, users={'chronos', 'crosvm',
-                                                'pluginvm'}),
-    GroupEntry(group='arc-bridge', gid=602, users={'chronos'}),
     GroupEntry(group='brltty', gid=240, users={'chronos'}),
+    GroupEntry(group='modem', gid=241, users={'shill'}),
+    GroupEntry(group='goofy', gid=248, users={'goofy'}),
+    GroupEntry(group='portage', gid=250, users={'portage'}),
     GroupEntry(group='preserve', gid=253, users={'root', 'attestation',
                                                  'tpm_manager'}),
-    GroupEntry(group='goofy', gid=248, users={'goofy'}),
     GroupEntry(group='authpolicyd', gid=254, users={'authpolicyd',
                                                     'authpolicyd-exec'}),
     GroupEntry(group='scanner', gid=255, users={'saned'}),
@@ -144,30 +143,27 @@ GROUP_BASELINE = dict((e.group, e) for e in (
     GroupEntry(group='buffet', gid=264, users={'chronos', 'buffet', 'power'}),
     GroupEntry(group='webservd', gid=266, users={'buffet', 'webservd'}),
     GroupEntry(group='lpadmin', gid=269, users={'cups', 'lpadmin'}),
+
+    # FUSE-based filesystem daemons.
     GroupEntry(group='policy-readers', gid=303, users={'attestation',
                                                        'authpolicyd', 'chronos',
                                                        'u2f', 'shill'}),
-    GroupEntry(group='ipsec', gid=212, users={'shill'}),
-    GroupEntry(group='debugfs-access', gid=605, users={'arc-camera', 'shill',
-                                                       'power', 'metrics',
-                                                       'traced-probes'}),
-    GroupEntry(group='arc-camera', gid=603, users={'chronos', 'crosvm'}),
+    GroupEntry(group='fuse-drivefs', gid=304, users={'chronos'}),
+    GroupEntry(group='rmtfs', gid=306, users={'rmtfs', 'shill'}),
+
+    # Groups with no associated user.
     GroupEntry(group='daemon-store', gid=400, users={'biod', 'chaps',
                                                      'crosvm', 'shill'}),
     GroupEntry(group='logs-access', gid=401, users={'debugd-logs'}),
     GroupEntry(group='serial', gid=402, users={'uucp'}),
+    GroupEntry(group='hidraw', gid=403, users={'fwupdate-hidraw'}),
     GroupEntry(group='i2c', gid=404, users={'fwupdate-i2c',
                                             'fwupdate-drm_dp_aux-i2c',
                                             'power'}),
-    GroupEntry(group='android-root', gid=655360, users={'android-root'}),
-    GroupEntry(group='android-everybody', gid=665357,
-               users={'chronos', 'cros-disks', 'seneschal'}),
-    GroupEntry(group='user-containers', gid=10000, users={'user-containers'}),
-    GroupEntry(group='midis', gid=608, users={'chronos'}),
-    GroupEntry(group='cfm-peripherals', gid=20103,
-               users={'cfm-monitor', 'cfm-firmware-updaters'}),
-    GroupEntry(group='ippusb', gid=20100, users={'ippusb', 'lp', 'lpadmin',
-                                                 'cups', 'saned'}),
+    GroupEntry(group='apex-access', gid=405, users={'rtanalytics'}),
+    GroupEntry(group='utmp', gid=406),
+    GroupEntry(group='drm_dp_aux', gid=407, users={'fwupdate-drm_dp_aux',
+                                                   'fwupdate-drm_dp_aux-i2c'}),
     GroupEntry(group='tun', gid=413, users={'crosvm', 'shill', 'vpn', 'wpan'}),
     GroupEntry(group='gpio', gid=414, users={'modem'}),
     GroupEntry(group='suzy-q', gid=415, users={'chronos', 'rma_fw_keeper'}),
@@ -175,34 +171,62 @@ GROUP_BASELINE = dict((e.group, e) for e in (
                                                        'healthd_ec',
                                                        'power',
                                                        'typecd_ec'}),
-    GroupEntry(group='shill', gid=20104, users={'shill', 'ipsec'}),
-    GroupEntry(group='fuse-drivefs', gid=304, users={'chronos'}),
+    GroupEntry(group='virtaccess', gid=418, users={'crosvm', 'wilco_dtc'}),
+    GroupEntry(group='crash-access', gid=419, users={'crash'}),
+    GroupEntry(group='crash-user-access', gid=420,
+               users={'crash', 'chronos', 'vm_cicerone'}),
+
+    GroupEntry(group='cras', gid=600, users={'chronos', 'crosvm', 'power',
+                                             'rtanalytics', 'sound_card_init'}),
+    GroupEntry(group='wayland', gid=601, users={'chronos', 'crosvm',
+                                                'pluginvm'}),
+    GroupEntry(group='arc-bridge', gid=602, users={'chronos'}),
+    GroupEntry(group='arc-camera', gid=603, users={'chronos', 'crosvm'}),
+    GroupEntry(group='debugfs-access', gid=605, users={'arc-camera', 'shill',
+                                                       'power', 'metrics',
+                                                       'traced-probes'}),
+    GroupEntry(group='midis', gid=608, users={'chronos'}),
     GroupEntry(group='password-viewers', gid=611, users={'kerberosd', 'shill',
                                                          'system-proxy'}),
-    GroupEntry(group='apex-access', gid=405, users={'rtanalytics'}),
-    GroupEntry(group='modem', gid=241, users={'shill'}),
+
+    GroupEntry(group='chronos', gid=1000),
+    GroupEntry(group='chronos-access', gid=1001,
+               users={'root', 'ipsec', 'chronos',
+                      'cros-disks', 'imageloaderd', 'crash', 'dlp'}),
+
+    GroupEntry(group='user-containers', gid=10000, users={'user-containers'}),
+
+    # More CrOS system daemon GIDs.
+    GroupEntry(group='ippusb', gid=20100, users={'ippusb', 'lp', 'lpadmin',
+                                                 'cups', 'saned'}),
+    GroupEntry(group='cfm-peripherals', gid=20103,
+               users={'cfm-monitor', 'cfm-firmware-updaters'}),
+    GroupEntry(group='shill', gid=20104, users={'shill', 'ipsec'}),
     GroupEntry(group='pluginvm', gid=20128, users={'crosvm', 'pluginvm'}),
-    GroupEntry(group='rmtfs', gid=306, users={'rmtfs', 'shill'}),
-    GroupEntry(group='virtaccess', gid=418, users={'crosvm', 'wilco_dtc'}),
     GroupEntry(group='kerberosd', gid=20131, users={'kerberosd',
                                                     'kerberosd-exec'}),
-    GroupEntry(group='hidraw', gid=403, users={'fwupdate-hidraw'}),
-    GroupEntry(group='drm_dp_aux', gid=407, users={'fwupdate-drm_dp_aux',
-                                                   'fwupdate-drm_dp_aux-i2c'}),
     GroupEntry(group='cups-proxy', gid=20136, users={'crosvm', 'cups-proxy',
                                                      'pluginvm'}),
     GroupEntry(group='usbprinter', gid=20155, users={'cups', 'saned'}),
+    GroupEntry(group='hotline', gid=20157, users={'hotline', 'hotlog'}),
     GroupEntry(group='traced-producer', gid=20162, users={'traced',
                                                           'traced-probes',
                                                           'chronos',
                                                           'crosvm'}),
     GroupEntry(group='traced-consumer', gid=20164, users={'traced',
                                                           'chronos'}),
-    GroupEntry(group='hotline', gid=20157, users={'hotline', 'hotlog'}),
     GroupEntry(group='vpn', gid=20174, users={'vpn', 'shill'}),
+
+    GroupEntry(group='nogroup', gid=65533),
+    GroupEntry(group='nobody', gid=65534),
+
+    GroupEntry(group='android-everybody', gid=665357,
+               users={'chronos', 'cros-disks', 'seneschal'}),
+    GroupEntry(group='android-root', gid=655360, users={'android-root'}),
 ))
 
 GROUP_BASELINE_LAKITU = dict((e.group, e) for e in (
+    # These are sorted by gid.
     GroupEntry(group='systemd-journal', gid=270),
     GroupEntry(group='systemd-timesync', gid=271),
     GroupEntry(group='systemd-network', gid=274),
@@ -212,18 +236,14 @@ GROUP_BASELINE_LAKITU = dict((e.group, e) for e in (
 ))
 
 GROUP_BASELINE_JETSTREAM = dict((e.group, e) for e in (
+    # These are sorted by gid.
     GroupEntry(group='leds', gid=1102, users={'ap-controller'}),
-    GroupEntry(group='wpa_supplicant', gid=1114,
-               users={'ap-wifi-diagnostics', 'wpa_supplicant',
-                      'ap-wifi-manager', 'ap-hal', 'ap-wireless-optimizer'}),
     GroupEntry(group='hostapd', gid=1106,
                users={'hostapd', 'ap-wireless-optimizer', 'ap-monitor',
                       'ap-wifi-manager', 'ap-wifi-diagnostics', 'ap-hal'}),
-    # Add users to gdisp group in order to allow those processes to access
-    # the unix domain socket file to communicate with gdisp-broker daemon
-    GroupEntry(group='gdisp', gid=2700,
-               users={'gdisp', 'ap-csi-collector', 'ap-csi-preproc',
-                      'ap-csi-inference'}),
+    GroupEntry(group='wpa_supplicant', gid=1114,
+               users={'ap-wifi-diagnostics', 'wpa_supplicant',
+                      'ap-wifi-manager', 'ap-hal', 'ap-wireless-optimizer'}),
     # Add gwifi users to a common gwifi group to allow access of some shared
     # resources by multiple users.
     GroupEntry(
@@ -244,14 +264,21 @@ GROUP_BASELINE_JETSTREAM = dict((e.group, e) for e in (
             'ap-update-manager', 'ap-vorlon-client', 'ap-wifi-diagnostics',
             'ap-wifi-manager', 'ap-wireless-optimizer', 'gdisp'
         }),
+    # Add users to gdisp group in order to allow those processes to access
+    # the unix domain socket file to communicate with gdisp-broker daemon
+    GroupEntry(group='gdisp', gid=2700,
+               users={'gdisp', 'ap-csi-collector', 'ap-csi-preproc',
+                      'ap-csi-inference'}),
 ))
 
 # rialtod:!:400:rialto
 GROUP_BASELINE_RIALTO = dict((e.group, e) for e in (
+    # These are sorted by gid.
     GroupEntry(group='rialtod', gid=400, users={'rialto'}),
 ))
 
 GROUP_BASELINE_TERMINA = dict((e.group, e) for e in (
+    # These are sorted by gid.
     GroupEntry(group='lxd', gid=298, users={'lxd', 'chronos'}),
 ))
 
