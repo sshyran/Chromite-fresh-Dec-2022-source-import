a61f65b97151bb69df11b31f2cfabb2fe53479f3
George Burgess IV <gbiv@google.com>
2021-03-19 11:56:10 -0700

    cros_setup_toolchains: force rust-bootstrap to use slot 0
    in order to generate rust-bootstrap prebuilts for 1.4{7,8,9}, we'll have
    multiple stable rust-bootstrap versions in portage's package graph at
    once. devs only need rust-bootstrap-1.46.0, which is in slot 0.
    
    BUG=chromium:1185882
    TEST=cros_setup_toolchains, sdk tryjob
    
    Change-Id: I2ec049c92a55af02c7992dfda55fd5ef88afe196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2775714
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- scripts/cros_setup_toolchains.py
+++ scripts/cros_setup_toolchains.py
@@ -73,7 +73,7 @@ HOST_PACKAGES = (
 # build), so we have to delay their installation.
 HOST_POST_CROSS_PACKAGES = (
     'dev-lang/rust',
-    'dev-lang/rust-bootstrap',
+    'dev-lang/rust-bootstrap:0',
     'virtual/target-sdk-post-cross',
     'dev-embedded/coreboot-sdk',
 )
