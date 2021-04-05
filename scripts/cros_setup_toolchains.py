388a6ce652b6856b7d73a98ca3742d46c9a4daae
George Burgess IV <gbiv@google.com>
2021-04-05 00:03:59 -0700

    cros_setup_toolchains: stop building special rust-bootstrap versions
    the builder's generated everything we need it to. this should be safe
    to remove now.
    
    BUG=chromium:1185882
    TEST=None
    
    Change-Id: I1ccc1b09f85eabd876f2ce4d55d9fd44d402bbbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2805320
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- scripts/cros_setup_toolchains.py
+++ scripts/cros_setup_toolchains.py
@@ -74,7 +74,6 @@ HOST_PACKAGES = (
 HOST_POST_CROSS_PACKAGES = (
     'dev-lang/rust',
     'dev-lang/rust-bootstrap:0',
-    'dev-lang/rust-bootstrap:1.49.0',
     'virtual/target-sdk-post-cross',
     'dev-embedded/coreboot-sdk',
 )
@@ -83,9 +82,7 @@ HOST_POST_CROSS_PACKAGES = (
 # bot hasn't had a chance to run yet, there are no binary packages available,
 # so we have to list them here and wait.  Once it completes, entries here can
 # be removed so they'll end up on bots & dev's systems.
-NEW_PACKAGES = (
-    'dev-lang/rust-bootstrap:1.49.0',
-)
+NEW_PACKAGES = ()
 
 # Enable the Go compiler for these targets.
 TARGET_GO_ENABLED = (
