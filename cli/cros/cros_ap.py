da5183e8b8e548187f124fbd01c5b86975283aa5
Sergey Frolov <sfrolov@google.com>
2021-06-02 16:34:02 -0600

    cros ap: print error if no subcommand provided
    BUG=None
    TEST=Manual
    
    Change-Id: Iac210e64a6f6c61fb510d9a3c7ec2ed3962cbf87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2935008
    Tested-by: Sergey Frolov <sfrolov@google.com>
    Auto-Submit: Sergey Frolov <sfrolov@google.com>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Commit-Queue: Raul E Rangel <rrangel@chromium.org>

--- cli/cros/cros_ap.py
+++ cli/cros/cros_ap.py
@@ -58,6 +58,10 @@ class APCommand(command.CliCommand):
       subcmd = BuildSubcommand(self.options)
     elif self.options.ap_command == COMMAND_FLASH:
       subcmd = FlashSubcommand(self.options)
+    else:
+      cros_build_lib.Die('Please specify one of ap subcommands: %s. '
+                         'Refer to `cros ap --help` for full help.' %
+                         [COMMAND_DUMP_CONFIG, COMMAND_BUILD, COMMAND_FLASH])
     subcmd.run()
 
 
