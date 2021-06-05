7d5c17f7eb4604ff3143d965bba34bc738efb459
Mike Frysinger <vapier@chromium.org>
2021-06-05 02:00:20 -0400

    image_test: start validating tmpfiles.d entries
    Unknown users & groups in tmpfiles.d can break booting.
    
    BUG=None
    TEST=CQ passes
    
    Force-Relevant-Builds: all
    Change-Id: Iadfea6dc0d5303f20685efa901692d279c7347fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/2941344
    Reviewed-by: Allen Webb <allenwebb@google.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- cros/test/image_test.py
+++ cros/test/image_test.py
@@ -17,8 +17,10 @@ import io
 import itertools
 import mimetypes
 import os
+from pathlib import Path
 import re
 import stat
+from typing import NamedTuple
 import unittest
 
 import magic  # pylint: disable=import-error
@@ -958,5 +960,66 @@ class DBusServiceTest(image_test_lib.ImageTestCase):
         success = False
         logging.error('%s: Add an Upstart script to manage D-Bus activated '
                       'service lifecycle: see crbug.com/1025914.', filename)
+
+    self.assertTrue(success)
+
+
+class TmpfilesdEntry(NamedTuple):
+  """An entry in a tmpfiles.d file."""
+  config: str
+  type: str
+  path: str
+  mode: str
+  user: str
+  group: str
+  age: str
+  argument: str
+
+
+class TmpfilesdTest(image_test_lib.ImageTestCase):
+  """Verify tmpfiles.d configuration settings."""
+
+  def _parse(self, conf):
+    """Parse a tmpfiles.d file and yield each entry."""
+    for line in conf.read_text('utf-8').splitlines():
+      line = line.strip().split('#', 1)[0]
+      if not line:
+        continue
+
+      line = line.split()
+      yield TmpfilesdEntry(conf, *(line + (['-'] * 5))[0:7])
+
+  def _iter_tmpfiles_d(self):
+    """Yield every tmpfiles.d entry in the rootfs."""
+    root = Path(image_test_lib.ROOT_A)
+    etc_tmpfiles_d = root / 'etc' / 'tmpfiles.d'
+    usr_tmpfiles_d = root / 'usr' / 'lib' / 'tmpfiles.d'
+    for tmpfiles_d in (etc_tmpfiles_d, usr_tmpfiles_d):
+      for conf in tmpfiles_d.glob('*.conf'):
+        yield from self._parse(conf)
+
+  def TestAccounts(self):
+    """Make sure every user & group actually exist.
+
+    If the accounts don't exist at runtime, tmpfiles.d likes to blow up.
+    """
+    root = Path(image_test_lib.ROOT_A)
+    etc_passwd = root / 'etc' / 'passwd'
+    etc_group = root / 'etc' / 'group'
+    valid_users = set(x.split(':', 1)[0]
+                      for x in etc_passwd.read_text('utf-8').splitlines())
+    valid_users.add('-')
+    valid_groups = set(x.split(':', 1)[0]
+                       for x in etc_group.read_text('utf-8').splitlines())
+    valid_groups.add('-')
+
+    success = True
+    for entry in self._iter_tmpfiles_d():
+      if entry.user not in valid_users:
+        logging.error('%s: unknown user', entry)
+        success = False
+      if entry.group not in valid_groups:
+        logging.error('%s: unknown group', entry)
+        success = False
 
     self.assertTrue(success)
