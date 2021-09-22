757887f7858d21e33d0ede582a0786ea5f4c7d61
George Burgess IV <gbiv@google.com>
2021-09-22 15:58:35 -0700

    Reland "xz_auto: use pixz for either compression or decompression"
    This is a reland of 407ec66d620adaa61d3e52f68114dae635cdfb54, with
    fixes rolled in.
    
    The fixes tweak how we hold pixz to support cros_build_lib's
    CompressFile invocation. Our previous behavior was that pixz would write
    to ${input_file}.xz, which may/may not be the file that cros_build_lib
    wanted it to write to.
    
    Original change's description:
    > xz_auto: use pixz for either compression or decompression
    >
    > On my machine, `xz -T0 foo.tar && pixz -d foo.tar.xz` results in
    > the multi-threaded compression of `foo.tar`, and the single-threaded
    > decompression thereof. If you replace `xz` with `pixz` for compression,
    > both compression and decompression are multi-threaded.
    >
    > This also makes us use a consistent number of threads; currently, we use
    > ncpu/2 for pixz, and ncpu for xz.
    >
    > This also adds tests for xz_auto, since we had none before.
    >
    > BUG=None
    > TEST=./run_tests
    >
    > Change-Id: Ia2d60d3fdee5ba34af9a75903cd8127e0b82c493
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3180382
    > Commit-Queue: George Burgess <gbiv@chromium.org>
    > Tested-by: George Burgess <gbiv@chromium.org>
    > Reviewed-by: Mike Frysinger <vapier@chromium.org>
    
    BUG=b:202735786
    TEST=./run_tests
    
    Change-Id: I7f822bcb1ba077d55a78361c228746e728be4eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3253127
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- scripts/xz_auto.py
+++ scripts/xz_auto.py
@@ -6,42 +6,135 @@
 
 from __future__ import division
 
-import multiprocessing
+import getopt
 import os
+import subprocess
+import sys
 
 from chromite.lib import commandline
 from chromite.lib import osutils
 from chromite.utils import memoize
 
 
+PIXZ_DISABLE_VAR = 'FOR_TEST_XZ_AUTO_NO_PIXZ'
+
+
 @memoize.Memoize
 def HasPixz():
   """Returns path to pixz if it's on PATH or None otherwise."""
-  return osutils.Which('pixz')
+  return PIXZ_DISABLE_VAR not in os.environ and osutils.Which('pixz')
 
 
-@memoize.Memoize
-def GetJobCount():
-  """Returns half of the total number of the machine's CPUs as a string.
+def ParsePixzArgs(argv):
+  """Determines flags to pass to pixz, per argv.
 
-  Returns half rather than all of them to avoid starving out other parallel
-  processes on the same machine.
+  Returns:
+    A tuple containing:
+    - A raw list of flags to pass to pixz.
+    - An optional input file.
+    - An optional output file (only exists if the input file is present).
   """
-  return str(int(max(1, multiprocessing.cpu_count() / 2)))
+  # Glancing at docs, the following opts are supported. -i and -o are ignored,
+  # since we assert in `main` that they're not present, but include parsing for
+  # them anyway.
+  flags, args = getopt.gnu_getopt(
+      args=argv,
+      shortopts='dlxi:o:0123456789p:tkch',
+  )
+  if not args:
+    file_to_compress = None
+    target = None
+  elif len(args) == 1:
+    file_to_compress = args[0]
+    target = None
+  else:
+    file_to_compress = args[0]
+    target = args[1]
+
+  raw_flag_list = []
+  for key, val in flags:
+    raw_flag_list.append(key)
+    if val:
+      raw_flag_list.append(val)
+
+  return raw_flag_list, file_to_compress, target
 
 
-def GetDecompressCommand(stdout):
-  """Returns decompression command."""
+def Execvp(argv):
+  """Execs the given argv."""
+  os.execvp(argv[0], argv)
+
+
+def ExecCompressCommand(stdout, argv):
+  """Execs compression command."""
+  # It appears that in order for pixz to do parallel decompression, compression
+  # needs to be done with pixz. xz itself is only capable of parallel
+  # compression.
+  if not HasPixz():
+    cmd = ['xz']
+
+    if stdout:
+      cmd.append('-zc')
+    else:
+      cmd.append('-z')
+    cmd += argv
+    Execvp(cmd)
+
+  cmd = ['pixz']
+  raw_flag_list, compressed_file_name, output_file = ParsePixzArgs(argv)
+
+  autodelete_input_file = False
+  if not compressed_file_name:
+    assert not output_file
+    compressed_file_name = '/dev/stdin'
+    output_file = '/dev/stdout'
+  elif stdout:
+    output_file = '/dev/stdout'
+  elif not output_file:
+    # Pixz defaults to a `.pxz` suffix (or `.tpxz` if it's compressing a
+    # tar file). We need the suffix to be consistent, so force it here.
+    output_file = f'{compressed_file_name}.xz'
+    autodelete_input_file = True
+
+  cmd += raw_flag_list
+  cmd.append(compressed_file_name)
+  if output_file:
+    cmd.append(output_file)
+
+  if not autodelete_input_file:
+    Execvp(cmd)
+
+  return_code = subprocess.call(cmd)
+  if not return_code:
+    os.unlink(compressed_file_name)
+  sys.exit(return_code)
+
+
+def ExecDecompressCommand(stdout, argv):
+  """Execs decompression command."""
   if HasPixz():
-    cmd = ['pixz', '-d', '-p', GetJobCount()]
+    cmd = ['pixz']
+    cmd.append('-d')
+
+    _, compressed_file_name, _ = ParsePixzArgs(argv)
     if stdout:
       # Explicitly tell pixz the file is the input, so it will dump the output
       # to stdout, instead of automatically choosing an output name.
       cmd.append('-i')
-    return cmd
+      if not compressed_file_name:
+        cmd.append('/dev/stdin')
+    elif not compressed_file_name:
+      cmd += ['-i', '/dev/stdin']
+    cmd += argv
+    Execvp(cmd)
+
+  cmd = ['xz']
   if stdout:
-    return ['xz', '-dc']
-  return ['xz', '-d']
+    cmd.append('-dc')
+  else:
+    cmd.append('-d')
+  cmd += argv
+  Execvp(cmd)
 
 
 def GetParser():
@@ -67,12 +160,7 @@ def main(argv):
   if '-i' in argv or '-o' in argv:
     parser.error('It is invalid to use -i or -o with xz_auto')
 
-  # xz doesn't support multi-threaded decompression, so try using pixz for that.
   if known_args.decompress:
-    args = GetDecompressCommand(known_args.stdout)
-    os.execvp(args[0], args + argv)
+    ExecDecompressCommand(known_args.stdout, argv)
   else:
-    cmd = ['xz', '-T0']
-    if known_args.stdout:
-      cmd.append('-c')
-    os.execvp(cmd[0], cmd + argv)
+    ExecCompressCommand(known_args.stdout, argv)
---
+++ scripts/xz_auto_unittest.py
@@ -0,0 +1,200 @@
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Unit tests for xz_auto.py."""
+
+import os
+from pathlib import Path
+import unittest
+from unittest import mock
+
+from chromite.lib import cros_build_lib
+from chromite.lib import cros_test_lib
+from chromite.lib import osutils
+from chromite.scripts import xz_auto
+
+
+DIR = Path(__file__).resolve().parent
+
+
+def FindXzAutoLocation():
+  """Figures out where the xz_auto binary is."""
+  return DIR / 'xz_auto'
+
+
+class XzAutoTests(cros_test_lib.MockTempDirTestCase):
+  """Various tests for xz_auto."""
+
+  def DisablePixzForCurrentTest(self):
+    """Disables the use of pixz for the current test."""
+    # This will be cleaned up by cros_test_lib, so no need to addCleanup.
+    os.environ[xz_auto.PIXZ_DISABLE_VAR] = '1'
+
+  def testPixzArgParsingSeemsToWork(self):
+    """Tests our detection of file names in pixz commandlines."""
+    self.assertEqual(
+        xz_auto.ParsePixzArgs(['to_compress.txt']),
+        ([], 'to_compress.txt', None),
+    )
+    self.assertEqual(
+        xz_auto.ParsePixzArgs(['to_compress.txt', 'compressed.txt']),
+        ([], 'to_compress.txt', 'compressed.txt'),
+    )
+    self.assertEqual(
+        xz_auto.ParsePixzArgs(['to_compress.txt', '-c', 'compressed.txt',
+                               '-9']),
+        (['-c', '-9'], 'to_compress.txt', 'compressed.txt'),
+    )
+    self.assertEqual(
+        xz_auto.ParsePixzArgs(
+            ['-t', 'to_compress.txt', '-c', 'compressed.txt', '-p', '2']),
+        (['-t', '-c', '-p', '2'], 'to_compress.txt', 'compressed.txt'),
+    )
+    self.assertEqual(
+        xz_auto.ParsePixzArgs(['-tcp2', 'to_compress.txt', 'compressed.txt']),
+        (['-t', '-c', '-p', '2'], 'to_compress.txt', 'compressed.txt'),
+    )
+
+  @unittest.skipIf(not xz_auto.HasPixz(), 'need pixz for this test')
+  @mock.patch.object(xz_auto, 'Execvp')
+  def testPixzCommandCreationSelectsPixzIfAvailable(self, execvp_mock):
+    """Tests that we actually execute pixz when we intend to."""
+    class ExecvpStopError(Exception):
+      """Convenient way to halt execution."""
+
+    def execvp_side_effect(argv):
+      """Does testing of our execvp calls."""
+      self.assertEqual(argv[0], 'pixz')
+      raise ExecvpStopError()
+
+    execvp_mock.side_effect = execvp_side_effect
+    with self.assertRaises(ExecvpStopError):
+      xz_auto.ExecCompressCommand(stdout=False, argv=[])
+
+    with self.assertRaises(ExecvpStopError):
+      xz_auto.ExecDecompressCommand(stdout=False, argv=[])
+
+  def _TestFileCompressionImpl(self):
+    """Tests that compressing a file with xz_auto WAI."""
+    file_contents = b'some random file contents'
+    file_location = os.path.join(self.tempdir, 'file.txt')
+    osutils.WriteFile(file_location, file_contents, mode='wb')
+
+    xz_auto_script = str(FindXzAutoLocation())
+    cros_build_lib.run(
+        [
+            xz_auto_script,
+            file_location,
+        ],
+        check=True,
+    )
+
+    xz_location = file_location + '.xz'
+    self.assertExists(xz_location)
+    self.assertNotExists(file_location)
+    cros_build_lib.run([
+        xz_auto_script,
+        '--decompress',
+        xz_location,
+    ])
+    self.assertNotExists(xz_location)
+    self.assertExists(file_location)
+    self.assertEqual(
+        osutils.ReadFile(file_location, mode='rb'),
+        file_contents,
+    )
+
+  def _TestStdoutCompressionImpl(self):
+    """Tests that compressing stdstreams with xz_auto WAI."""
+    file_contents = b'some random file contents'
+    xz_auto_script = str(FindXzAutoLocation())
+
+    run_result = cros_build_lib.run(
+        [
+            xz_auto_script,
+            '-c',
+        ],
+        capture_output=True,
+        input=file_contents,
+    )
+
+    compressed_file = run_result.stdout
+    self.assertNotEqual(compressed_file, file_contents)
+
+    run_result = cros_build_lib.run(
+        [
+            xz_auto_script,
+            '--decompress',
+            '-c',
+        ],
+        input=compressed_file,
+        capture_output=True,
+    )
+    uncompressed_file = run_result.stdout
+    self.assertEqual(file_contents, uncompressed_file)
+
+  def _TestStdoutCompressionFromFileImpl(self):
+    """Tests that compression of a file & outputting to stdout works.
+
+    Pixz has some semi-weird behavior here (b/202735786).
+    """
+    file_contents = b'some random file contents'
+    xz_auto_script = str(FindXzAutoLocation())
+    file_location = os.path.join(self.tempdir, 'file.txt')
+    osutils.WriteFile(file_location, file_contents, mode='wb')
+
+    run_result = cros_build_lib.run(
+        [
+            xz_auto_script,
+            '-c',
+            file_location,
+        ],
+        capture_output=True,
+    )
+
+    compressed_file = run_result.stdout
+    self.assertExists(file_location)
+    self.assertNotEqual(compressed_file, file_contents)
+
+    run_result = cros_build_lib.run(
+        [
+            xz_auto_script,
+            '--decompress',
+            '-c',
+        ],
+        capture_output=True,
+        input=compressed_file,
+    )
+    uncompressed_file = run_result.stdout
+    self.assertEqual(file_contents, uncompressed_file)
+
+  @unittest.skipIf(not xz_auto.HasPixz(), 'need pixz for this test')
+  def testFileCompressionWithPixzWorks(self):
+    """Tests that compressing a file with pixz WAI."""
+    self._TestFileCompressionImpl()
+
+  @unittest.skipIf(not xz_auto.HasPixz(), 'need pixz for this test')
+  def testStdoutCompressionWithPixzWorks(self):
+    """Tests that compressing `stdout` with pixz WAI."""
+    self._TestStdoutCompressionImpl()
+
+  @unittest.skipIf(not xz_auto.HasPixz(), 'need pixz for this test')
+  def testStdoutCompressionFromFileWithPixzWorks(self):
+    """Tests that compressing from a file to stdout with pixz WAI."""
+    self._TestStdoutCompressionFromFileImpl()
+
+  def testFileCompressionWithXzWorks(self):
+    """Tests that compressing a file with pixz WAI."""
+    self.DisablePixzForCurrentTest()
+    self._TestFileCompressionImpl()
+
+  def testStdoutCompressionWithXzWorks(self):
+    """Tests that compressing `stdout` with pixz WAI."""
+    self.DisablePixzForCurrentTest()
+    self._TestStdoutCompressionImpl()
+
+  def testStdoutCompressionFromFileWithXzWorks(self):
+    """Tests that compressing from a file to stdout WAI."""
+    self.DisablePixzForCurrentTest()
+    self._TestStdoutCompressionFromFileImpl()
