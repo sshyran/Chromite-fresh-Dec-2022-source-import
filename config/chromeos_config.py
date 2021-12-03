eae3e825068727957f601ffca19b5463ebe5f283
Knox Chiou <knoxchiou@google.com>
2021-12-03 15:24:05 +0800

    kukui/jacuzzi: add factory branch builder
    Add kukui/jacuzzi factory branch builder,
    running at daily schedule.
    This factory branch is based on kernel v5.10.
    
    BUG=b:208940466
    TEST=./refresh_generated_files
    
    Change-Id: Ibb999353debf9025d1818893350ed0e2e39c50ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3314438
    Tested-by: Knox Chiou <knoxchiou@chromium.org>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Reviewed-by: Jack Neus <jackneus@google.com>
    Commit-Queue: Knox Chiou <knoxchiou@chromium.org>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -1781,6 +1781,7 @@ def FactoryBuilders(site_config, _boards_dict, _ge_build_config):
       (WEEKLY, 'factory-puff-13813.B', ['puff']),
       (WEEKLY, 'factory-asurada-13929.B', ['asurada']),
       (WEEKLY, 'factory-ambassador-14265.B', ['ambassador']),
+      (DAILY, 'factory-kukui-14374.B', ['kukui', 'jacuzzi']),
       # This is intended to create master branch tryjobs, NOT for production
       # builds. Update the associated list of boards as needed.
       (None, 'master', [
