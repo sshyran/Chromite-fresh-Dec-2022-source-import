01bc84f557c611584fabe89a3ef704f9eb92a289
YH Lin <yueherngl@google.com>
2021-12-14 18:41:27 -0800

    brya: enable hw_tests
    Enable hw_tests which was disabled at the beginning.
    
    BUG=b:210754894
    TEST=refresh_generated_files
    
    Change-Id: I745f42e5e84659af54de8e1b633e07728da8eab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3340325
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Tested-by: YH Lin <yueherngl@chromium.org>
    Auto-Submit: YH Lin <yueherngl@chromium.org>
    Reviewed-by: George Engelbrecht <engeg@google.com>
    Commit-Queue: George Engelbrecht <engeg@google.com>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2462,17 +2462,9 @@ def ApplyCustomOverrides(site_config, ge_build_config):
       },
       'brya-release': {
           'sign_types': ['recovery', 'factory'],
-          # Brya has no DUTs in the lab (b/167721012).
-          'hw_tests': [],
-          'hw_tests_override': [],
-          'hw_tests_disabled_bug': 'b/167721012',
       },
       'brya-lvm-stateful-release': {
           'sign_types': ['recovery', 'factory'],
-          # Brya has no DUTs in the lab (b/167721012).
-          'hw_tests': [],
-          'hw_tests_override': [],
-          'hw_tests_disabled_bug': 'b/167721012',
       },
       'keeby-release': {
           'sign_types': ['recovery', 'factory'],
