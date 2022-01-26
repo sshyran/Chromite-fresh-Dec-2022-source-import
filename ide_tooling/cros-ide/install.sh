b868a96cdd2cd34718f7f950001e55322198fcd2
Keigo Oka <oka@google.com>
2022-01-26 10:27:04 +0000

    Fix lint error in install.sh
    BUG=None
    TEST=repo upload
    
    Change-Id: I716f32f6c126c291940d977369491d6fca0ba8a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/ide_tooling/+/3412403
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/install.sh
+++ ide_tooling/cros-ide/install.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || exit 1
 
 npm list typescript || npm install typescript
 npx ts-node ./src/tools/install.ts "$*"
