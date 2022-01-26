9dd62d3414547ff872d685df241035613f1333a9
Yoshiki Iguchi <yoshiki@google.com>
2022-01-26 16:59:50 +0900

    Add a test for execFile in ideutilities.ts
    BUG=b:216406932
    TEST=manually ran 'Extension Tests' on the IDE
    
    Change-Id: If8dfccec5d07ccbaaebf823decfe02820d31a459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/ide_tooling/+/3416891
    Reviewed-by: Eric Lok <lokeric@google.com>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Tested-by: Yoshiki Iguchi <yoshiki@chromium.org>

---
+++ ide_tooling/cros-ide/src/test/suite/ide_utilities.test.ts
@@ -0,0 +1,17 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import * as ide_utilities from '../../ide_utilities'
+
+suite('Extension Test Suite', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+
+  test('execFile', async () => {
+    const result = await ide_utilities.execFile('/bin/echo', ['hoge']);
+    assert.strictEqual('', result.stderr);
+    assert.strictEqual('hoge\n', result.stdout);
+  });
+});
