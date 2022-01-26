7a8910294e8d8061f6f74ece8769998bab124f0f
Keigo Oka <oka@google.com>
2022-01-26 07:19:41 +0000

    Make install.ts fail-safe
    This patch makes `install.ts --upload` check that the working directory
    is clean, append commit hash to the uploaded file for
    reproduciblity, and make sure the version is updated.
    
    BUG=b:207810586, b:214325959
    TEST=If version is not updated, upload fails.
    TEST=If the workspace is dirty, upload fails.
    TEST=If HEAD is not cros/main, upload fails.
    TEST=With IDE_IGNORE_CROS_MAIN_FOR_TESTING=1 upload succeeds.
    TEST=./install.sh works.
    
    Change-Id: Ib4633a3d5d3122240a448df23ef2f361a7a54c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/ide_tooling/+/3412399
    Reviewed-by: Eric Lok <lokeric@google.com>
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

--- ide_tooling/cros-ide/src/tools/install.ts
+++ ide_tooling/cros-ide/src/tools/install.ts
@@ -15,63 +15,117 @@ import * as commonUtil from '../common/common_util';
 
 function assertInsideChroot() {
   if (!commonUtil.isInsideChroot()) {
-    throw 'Error: not inside chroot'
+    throw new Error('not inside chroot');
   }
 }
 
-const GS_PREFIX = 'gs://chromeos-velocity/ide/cros-ide/'
+const GS_PREFIX = 'gs://chromeos-velocity/ide/cros-ide';
 
-async function execute(cmd: string) {
-  const { stdout, stderr } = await util.promisify(childProcess.exec)(cmd)
-  process.stderr.write(stderr)
-  process.stdout.write(stdout)
-  return stdout
+async function execute(cmd: string, showStdout?: boolean) {
+  const { stdout, stderr } = await util.promisify(childProcess.exec)(cmd);
+  process.stderr.write(stderr);
+  if (showStdout) {
+    process.stdout.write(stdout);
+  }
+  return stdout;
+}
+
+async function latestVersionUrl() {
+  // The result of `gsutil ls` is lexicographically sorted.
+  const stdout = await execute(`gsutil ls ${GS_PREFIX}`);
+  return stdout.trim().split("\n").pop()!;
+}
+
+async function gitIsDirty() {
+  const stdout = await execute(`git diff --stat`);
+  return stdout !== '';
+}
+
+// Assert the working directory is clean and get git commit hash.
+async function cleanCommitHash() {
+  if (await gitIsDirty()) {
+    throw new Error('dirty git status; run the command in clean environment');
+  }
+  const wantHash = await execute('git rev-parse cros/main');
+  const hash = await execute('git rev-parse HEAD');
+
+  if (process.env.IDE_IGNORE_CROS_MAIN_FOR_TESTING) {
+    // Skip check for manual testing of the script.
+  } else {
+    if (hash !== wantHash) {
+      throw new Error('HEAD must be cros/main');
+    }
+  }
+  return hash;
+}
+
+class Archive {
+  constructor(readonly name: string, readonly hash: string) {
+  }
+  url() {
+    return `${GS_PREFIX}/${this.name}@${this.hash}`;
+  }
+  static parse(url: string) {
+    const base = path.basename(url);
+    const [name, hash] = base.split('@');
+    return new Archive(name, hash);
+  }
 }
 
 async function buildAndUpload() {
-  let td: string | undefined
+  let latestInGs = Archive.parse(await latestVersionUrl());
+  let hash = await cleanCommitHash();
+  let td: string | undefined;
   try {
-    td = await fs.promises.mkdtemp(os.tmpdir() + '/')
-    await execute(`npx vsce@1.103.1 package -o ${td}/`)
-    await execute(`gsutil cp ${td}/* ${GS_PREFIX}`)
+    td = await fs.promises.mkdtemp(os.tmpdir() + '/');
+    await execute(`npx vsce@1.103.1 package -o ${td}/`);
+    const localName = (await fs.promises.readdir(td))[0];
+    if (latestInGs.name >= localName) {
+      throw new Error(`${localName} is older than the latest published version ${latestInGs.name}. Update the version and rerun the program.`);
+    }
+    const url = new Archive(localName, hash).url();
+    // TODO(oka): use execFile.
+    await execute(`gsutil cp ${td}/${localName} ${url}`);
   } finally {
     if (td) {
-      await fs.promises.rmdir(td, { recursive: true })
+      await fs.promises.rmdir(td, { recursive: true });
     }
   }
 }
 
 async function install() {
-  assertInsideChroot()
-  // The result of `gsutil ls` is lexicographically sorted.
-  const stdout = await execute(`gsutil ls ${GS_PREFIX}`)
-  const src = stdout.trim().split("\n").pop()!
+  assertInsideChroot();
 
-  let td: string | undefined
+  const src = await latestVersionUrl();
+  let td: string | undefined;
   try {
-    td = await fs.promises.mkdtemp(os.tmpdir() + '/')
-    const dst = path.join(td, path.basename(src))
+    td = await fs.promises.mkdtemp(os.tmpdir() + '/');
+    const dst = path.join(td, Archive.parse(src).name);
 
-    await execute(`gsutil cp ${src} ${dst}`)
-    await execute(`code --install-extension ${dst}`)
+    await execute(`gsutil cp ${src} ${dst}`);
+    await execute(`code --install-extension ${dst}`, true);
   } finally {
     if (td) {
-      await fs.promises.rmdir(td, { recursive: true })
+      await fs.promises.rmdir(td, { recursive: true });
     }
   }
 }
 
 async function main() {
-  const upload = process.argv.includes('--upload')
+  const upload = process.argv.includes('--upload');
   if (upload) {
-    await buildAndUpload()
-    return
+    await buildAndUpload();
+    return;
   }
-  await install()
+  try {
+    await install();
+  } catch (e) {
+    const message = (e as Error).message
+    throw new Error(`${message}\nRead quickstart.md and run the script in proper environment`);
+  }
 }
 
 main().catch(e => {
-  console.error(e)
-  console.error(`Read quickstart.md and run the script in proper environment`)
-  process.exit(1)
+  console.error(e);
+  process.exit(1);
 })
