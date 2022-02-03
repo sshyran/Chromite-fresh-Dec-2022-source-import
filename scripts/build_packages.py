fba23bac77ca1ac6f14e7d2b72c486d8aacdb068
Alex Klein <saklein@chromium.org>
2022-02-03 11:58:48 -0700

    build_packages: Display tree status on failure
    Display the tree status on failures to alert devs to known
    issues to avoid unnecessary toil and confusion.
    
    BUG=b:217739761
    TEST=manual
    
    Change-Id: Ifc0d55465a204b82e9cbacdc687e97384bfc8023
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3437295
    Tested-by: Alex Klein <saklein@chromium.org>
    Auto-Submit: Alex Klein <saklein@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- scripts/build_packages.py
+++ scripts/build_packages.py
@@ -10,7 +10,10 @@ The output packages will be used by the build_image script to create a
 bootable Chrome OS image.
 """
 
+import logging
 import os
+import urllib.error
+import urllib.request
 
 from chromite.lib import commandline
 from chromite.lib import constants
@@ -30,4 +33,10 @@ def main(argv):
     # TODO(b/187793559): Don't pass in print_cmd once we switch to argparse
     cros_build_lib.dbg_run(cmd, print_cmd=False)
   except cros_build_lib.RunCommandError as e:
+    try:
+      request = urllib.request.urlopen(
+          'https://chromiumos-status.appspot.com/current?format=raw')
+      logging.notice('Tree Status: %s', request.read().decode())
+    except urllib.error.HTTPError:
+      pass
     cros_build_lib.Die(e)
