6fe96f53ca5e07796e82033de542bda7e8ad2148
Tomasz Tylenda <ttylenda@chromium.org>
2022-02-25 12:09:32 +0000

    cros-ide: find coverage.json and use it.
    We base displayed decorations on the data that was found, but conversion
    to the right format has not been done yet, so the output is not correct.
    
    BUG=b:214322618
    TEST=manual
    
    Change-Id: I4156439f446de03ebdbf3f5bc3ad1f98a09f25f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3488658
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/coverage.ts
+++ ide_tooling/cros-ide/src/coverage.ts
@@ -1,6 +1,11 @@
 // Copyright 2022 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import * as fs from 'fs';
+import * as glob from 'glob';
+import * as path from 'path';
+import * as util from 'util';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -18,24 +23,20 @@ export function activate(context: vscode.ExtensionContext) {
 
   let activeEditor = vscode.window.activeTextEditor;
 
-  function updateDecorations() {
+  async function updateDecorations() {
     if (!activeEditor) {
       return;
     }
 
-    // We hard-code two ranges to demonstrate the UI.
+    const {covered: coveredRanges, uncovered: uncoveredRanges} =
+        await readDocumentCoverage(activeEditor.document.fileName);
 
-    activeEditor.setDecorations(
-        coveredDecoration,
-        [{
-          range: new vscode.Range(10, 0, 15, Number.MAX_VALUE),
-        }]);
-
-    activeEditor.setDecorations(
-        uncoveredDecoration,
-        [{
-          range: new vscode.Range(18, 0, 25, Number.MAX_VALUE),
-        }]);
+    if (coveredRanges) {
+      activeEditor.setDecorations(coveredDecoration, coveredRanges);
+    }
+    if (uncoveredRanges) {
+      activeEditor.setDecorations(uncoveredDecoration, uncoveredRanges);
+    }
   }
 
   updateDecorations();
@@ -44,4 +45,124 @@ export function activate(context: vscode.ExtensionContext) {
     activeEditor = editor;
     updateDecorations();
   });
+}
+
+/** Ranges where coverage decorations should be applied. */
+export interface Coverage {
+  covered?: vscode.Range[];
+  uncovered?: vscode.Range[];
+}
+
+/**
+ * Find coverage data for a given file. Returns undefined if coverage is
+ * not available, or ranges that should be shown.
+ */
+export async function readDocumentCoverage(
+    documentFileName : string, rootForTesting: string = '/'): Promise<Coverage> {
+  const {pkg, relativePath} = parseFileName(documentFileName);
+  if (!pkg || !relativePath) {
+    return {};
+  }
+
+  const coverageJson = await readPkgCoverage(pkg, rootForTesting);
+  if (!coverageJson) {
+    return {};
+  }
+
+  const segments = await getSegments(coverageJson, relativePath);
+  if (!segments) {
+    return {};
+  }
+
+  // TODO(ttylenda): process segments to display correct output
+
+  const coveredRanges : vscode.Range[] = [];
+  const uncoveredRanges : vscode.Range[] = [];
+
+  for (const s of segments) {
+    const line = s[LINE_NUMBER];
+    const range = new vscode.Range(line, 0, line, Number.MAX_VALUE);
+    (s[COUNT] > 0 ? coveredRanges : uncoveredRanges).push(range);
+  }
+
+  return {covered: coveredRanges, uncovered: uncoveredRanges};
+}
+
+/**
+ * LLVM's coverage format.
+ *
+ * Fields:
+ *   number - the line where this segment begins
+ *   column - the column where this segment begins
+ *   count - the execution count, or zero if no count was recorded
+ *   hasCount - when false, the segment was uninstrumented or skipped
+ *   IsRegionEntry - whether this enters a new region or returns
+ *                   to a previous count
+ */
+type Segment = [number, number, number, boolean, boolean, boolean?];
+
+const LINE_NUMBER = 0;
+const COUNT = 2;
+
+/** Actual coverage data that we need. */
+interface FileCoverage {
+  filename: string,
+  segments: Segment[],
+}
+
+/** Top-level element in coverage.json */
+interface CoverageJson {
+  // Only data[0] appears to be used.
+  data : {files: FileCoverage[]}[],
+}
+
+const platform2 = 'platform2/';
+
+/** Get package name and relative path from a path to platform2 file. */
+function parseFileName(documentFileName: string): {pkg?: string, relativePath?: string} {
+  const p2idx = documentFileName.lastIndexOf(platform2);
+  if (p2idx === -1) {
+    return {};
+  }
+  // TODO(ttylenda): Get the package without guessing ebuild name and globbing.
+  const relativePath = documentFileName.substring(p2idx + platform2.length);
+  const pkg = relativePath.split('/')[0];
+  return {pkg, relativePath};
+}
+
+// TODO(ttylenda): Decide if we need a specific board or can we use whatever is available in chroot.
+const coverageDir = 'build/amd64-generic/build/coverage_data/';
+
+/** Read coverage.json of a package. */
+async function readPkgCoverage(pkg: string, rootForTesting: string = '/')
+    : Promise<CoverageJson|undefined> {
+  const globPattern = `${path.join(rootForTesting, coverageDir)}*/${pkg}*/*/coverage.json`;
+  let matches: string[];
+  try {
+    matches = await util.promisify(glob)(globPattern);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+  const [coverageJson] = matches;
+  if (!coverageJson) {
+    return undefined;
+  }
+
+  try {
+    const coverageContents = await fs.promises.readFile(coverageJson, 'utf8');
+    return JSON.parse(coverageContents) as CoverageJson;
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+/** Get segments data from a coverage JSON object. */
+async function getSegments(coverage: CoverageJson, relativePath: string)
+    : Promise<Segment[]|undefined> {
+  const files = coverage.data[0].files;
+  // TODO(ttylenda): Find the right file in a more accurate way.
+  const currentFile = files.find(f => f.filename.endsWith(relativePath));
+  return currentFile && currentFile.segments;
 }
