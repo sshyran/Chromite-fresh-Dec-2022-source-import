87c4dbf0e0345e9d188850d34c7acd92c596db44
George Burgess IV <gbiv@google.com>
2022-03-21 12:05:11 -0700

    cr-os: make `enumerate_hot_packages` look at `env` files
    In cases like portage-stable packages, we can't simply modify the
    ebuilds, so we need to hack on these env files instead. Make tooling
    recognize this.
    
    BUG=b:225171713
    TEST=Ran on a local tree; it DTRT
    
    Change-Id: I729bdc563717a1aa0d962d5021c4003dedad7b14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3539577
    Reviewed-by: Chris McDonald <cjmcdonald@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Tested-by: George Burgess <gbiv@chromium.org>

--- scripts/enumerate_hot_packages.py
+++ scripts/enumerate_hot_packages.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Enumerates all Chrome OS packages that are marked as `hot`.
+"""Determines all Chrome OS packages that are marked as `hot`.
 
 Dumps results as a list of package names to a JSON file. Hotness is
 determined by statically analyzing an ebuild.
@@ -12,67 +12,69 @@ Primarily intended for use by the Chrome OS toolchain team.
 
 import json
 import logging
-import os
+from pathlib import Path
+from typing import Iterable, List, Tuple
 
 from chromite.lib import commandline
 from chromite.lib import portage_util
 
 
-def is_ebuild_marked_hot(ebuild_path):
-  with open(ebuild_path) as f:
-    # The detection of this is intentionally super simple.
-    #
-    # It's important to note that while this is a function, we also use it in
-    # comments in packages that are forcibly optimized for speed in some other
-    # way, like chromeos-chrome.
-    return any('cros_optimize_package_for_speed' in line for line in f)
+def contains_ebuild_hot_marker(file_path: Path):
+  """Returns whether a file seems to be marked as hot."""
+  ebuild_contents = file_path.read_text(encoding='utf-8')
+  return 'cros_optimize_package_for_speed' in ebuild_contents
 
 
-def enumerate_package_ebuilds():
+def locate_all_package_ebuilds(
+    overlays: Iterable[Path]) -> Iterable[Tuple[Path, str, List[Path]]]:
   """Determines package -> ebuild mappings for all packages.
 
   Yields a series of (package_path, package_name, [path_to_ebuilds]). This may
   yield the same package name multiple times if it's available in multiple
   overlays.
   """
-  for overlay in portage_util.FindOverlays(overlay_type='both'):
-    logging.debug('Found overlay %s', overlay)
-
+  for overlay in overlays:
     # Note that portage_util.GetOverlayEBuilds can't be used here, since that
     # specifically only searches for cros_workon candidates. We care about
     # everything we can possibly build.
-    for dir_path, dir_names, file_names in os.walk(overlay):
-      ebuilds = [x for x in file_names if x.endswith('.ebuild')]
-      if not ebuilds:
-        continue
-
-      # os.walk directly uses `dir_names` to figure out what to walk next. If
-      # there are ebuilds here, walking any lower is a waste, so don't do it.
-      del dir_names[:]
-
-      ebuild_dir = os.path.basename(dir_path)
-      ebuild_parent_dir = os.path.basename(os.path.dirname(dir_path))
-      package_name = '%s/%s' % (ebuild_parent_dir, ebuild_dir)
-      yield dir_path, package_name, ebuilds
+    for package_dir in overlay.glob('*/*'):
+      ebuilds = list(package_dir.glob('*.ebuild'))
+      if ebuilds:
+        package_name = f'{package_dir.parent.name}/{package_dir.name}'
+        yield package_dir, package_name, ebuilds
 
 
-def main(argv):
+def locate_all_hot_env_packages(overlays: Iterable[Path]) -> Iterable[str]:
+  """Yields packages marked hot by files in chromeos/config/env."""
+  for overlay in overlays:
+    env_dir = overlay / 'chromeos' / 'config' / 'env'
+    for f in env_dir.glob('*/*'):
+      if f.is_file() and contains_ebuild_hot_marker(f):
+        yield f'{f.parent.name}/{f.name}'
+
+
+def main(argv: List[str]):
   parser = commandline.ArgumentParser(description=__doc__)
-  parser.add_argument('--output', required=True)
+  parser.add_argument('--output', required=True, type=Path)
   opts = parser.parse_args(argv)
 
   ebuilds_found = 0
   packages_found = 0
   merged_packages = 0
-  mappings = {}
-  for package_dir, package, ebuilds in enumerate_package_ebuilds():
+
+  overlays = portage_util.FindOverlays(overlay_type='both')
+  logging.debug('Found overlays %s', overlays)
+  overlays = [Path(x) for x in overlays]
+
+  mappings = {x: True for x in locate_all_hot_env_packages(overlays)}
+  logging.debug('Found hot packages %r from env search', sorted(mappings))
+  for package_dir, package, ebuilds in locate_all_package_ebuilds(overlays):
     packages_found += 1
     ebuilds_found += len(ebuilds)
     logging.debug('Found package %r in %r with ebuilds %r', package,
                   package_dir, ebuilds)
 
-    is_marked_hot = any(
-        is_ebuild_marked_hot(os.path.join(package_dir, x)) for x in ebuilds)
+    is_marked_hot = any(contains_ebuild_hot_marker(x) for x in ebuilds)
     if is_marked_hot:
       logging.debug('Package is marked as hot')
     else:
@@ -93,5 +95,4 @@ def main(argv):
   logging.info('%d packages merged', merged_packages)
   logging.info('%d hot packages found, total', len(hot_packages))
 
-  with open(opts.output, 'w') as f:
-    json.dump(hot_packages, f)
+  opts.output.write_text(json.dumps(hot_packages), encoding='utf-8')
