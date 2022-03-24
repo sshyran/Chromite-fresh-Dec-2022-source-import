0dc476024d5483de2189d1013c8e193b0667549a
Tomasz Tylenda <ttylenda@chromium.org>
2022-03-24 15:11:19 +0900

    cros-ide: test searching for selected text in CodeSearch
    BUG=b:225282811
    TEST=manually verified that CS still works, including changing settings
    TEST=removed vscode.env.openExternal and verified the the test fails
    
    Change-Id: I7d120dbd2fc34751083eae5bb80d894ca9515c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3546209
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/features/codesearch.ts
+++ ide_tooling/cros-ide/src/features/codesearch.ts
@@ -6,14 +6,17 @@ import * as commonUtil from '../common/common_util';
 import * as ideUtilities from '../ide_utilities';
 
 export function activate(context: vscode.ExtensionContext) {
-  const openFileCmd =
-    vscode.commands.registerTextEditorCommand(
-        'cros-ide.codeSearchOpenCurrentFile',
-        openCurrentFile);
+  const codeSearch = new CodeSearch();
+
+  const openFileCmd = vscode.commands.registerTextEditorCommand(
+      'cros-ide.codeSearchOpenCurrentFile',
+      (textEditor: vscode.TextEditor) => codeSearch.openCurrentFile(textEditor),
+  );
 
   const searchSelectionCmd = vscode.commands.registerTextEditorCommand(
       'cros-ide.codeSearchSearchForSelection',
-      searchSelection);
+      (textEditor: vscode.TextEditor) => codeSearch.searchSelection(textEditor),
+  );
 
   context.subscriptions.push(openFileCmd, searchSelectionCmd);
 }
@@ -21,51 +24,65 @@ export function activate(context: vscode.ExtensionContext) {
 const generateCsPath = '~/chromiumos/chromite/contrib/generate_cs_path';
 const codeSearch = 'codeSearch';
 
-async function openCurrentFile(textEditor: vscode.TextEditor) {
-  const fullpath = textEditor.document.fileName;
+class CodeSearch {
+  constructor(
+    // WorkspaceConfiguration objects do not change when users change settings
+    // so we need to obtain them every time the user opens a CS link.
+    private readonly getConfigRoot:
+        () => vscode.WorkspaceConfiguration = ideUtilities.getConfigRoot,
+    private readonly openExternal: (uri: vscode.Uri) => void = vscode.env.openExternal,
+  ) {}
 
-  // Which CodeSearch to use, options are public, internal, or gitiles.
-  const csInstance = ideUtilities.getConfigRoot().get<string>(codeSearch);
+  async openCurrentFile(textEditor: vscode.TextEditor) {
+    const fullpath = textEditor.document.fileName;
 
-  const line = textEditor.selection.active.line + 1;
+    // Which CodeSearch to use, options are public, internal, or gitiles.
+    const csInstance = this.getConfigRoot().get<string>(codeSearch);
 
-  // generate_cs_path is a symlink that uses a wrapper to call a Python script,
-  // so we need to spawn a shell.
-  const res = await commonUtil.exec('sh',
-      ['-c', `${generateCsPath} --show "--${csInstance}" --line=${line} "${fullpath}"`],
-      undefined,
-      {ignoreNonZeroExit: true});
+    const line = textEditor.selection.active.line + 1;
 
-  if (res instanceof Error) {
-    vscode.window.showErrorMessage('Could not run generate_cs_path:' + res);
-    return;
+    // generate_cs_path is a symlink that uses a wrapper to call a Python script,
+    // so we need to spawn a shell.
+    const res = await commonUtil.exec('sh',
+        ['-c', `${generateCsPath} --show "--${csInstance}" --line=${line} "${fullpath}"`],
+        undefined,
+        {ignoreNonZeroExit: true});
+
+    if (res instanceof Error) {
+      vscode.window.showErrorMessage('Could not run generate_cs_path:' + res);
+      return;
+    }
+
+    const {exitStatus, stdout, stderr} = res;
+    if (exitStatus) {
+      vscode.window.showErrorMessage(`generate_cs_path returned an error: ${stderr}`);
+      return;
+    }
+
+    // trimEnd() to get rid of the newline.
+    this.openExternal(vscode.Uri.parse(stdout.trimEnd()));
   }
 
-  const {exitStatus, stdout, stderr} = res;
-  if (exitStatus) {
-    vscode.window.showErrorMessage(`generate_cs_path returned an error: ${stderr}`);
-    return;
-  }
+  // TODO: Figure out if the search should be limited to the current repo.
+  searchSelection(textEditor: vscode.TextEditor) {
+    if (textEditor.selection.isEmpty) {
+      return;
+    }
 
-  // trimEnd() to get rid of the newline.
-  vscode.env.openExternal(vscode.Uri.parse(stdout.trimEnd()));
+    // If the setting is gitiles, we use public CodeSearch
+    const csInstance = this.getConfigRoot().get<string>(codeSearch);
+    const csBase =
+      csInstance === 'internal' ?
+          'https://source.corp.google.com/' : 'https://source.chromium.org/';
+
+    const selectedText = textEditor.document.getText(textEditor.selection);
+    const uri =
+        vscode.Uri.parse(csBase)
+            .with({path: '/search', query: `q=${selectedText}`});
+    this.openExternal(uri);
+  }
 }
 
-// TODO: Figure out if the search should be limited to the current repo.
-function searchSelection(textEditor: vscode.TextEditor) {
-  if (textEditor.selection.isEmpty) {
-    return;
-  }
-
-  // If the setting is gitiles, we use public CodeSearch
-  const csInstance = ideUtilities.getConfigRoot().get<string>(codeSearch);
-  const csBase =
-    csInstance === 'internal' ?
-        'https://source.corp.google.com/' : 'https://source.chromium.org/';
-
-  const selectedText = textEditor.document.getText(textEditor.selection);
-  const uri =
-      vscode.Uri.parse(csBase)
-          .with({path: '/search', query: `q=${selectedText}`});
-  vscode.env.openExternal(uri);
-}
+export const TEST_ONLY = {
+  CodeSearch,
+};
---
+++ ide_tooling/cros-ide/src/test/integration/codesearch.test.ts
@@ -0,0 +1,79 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+import * as codesearch from '../../features/codesearch';
+
+const {CodeSearch} = codesearch.TEST_ONLY;
+
+/**
+ * Fake implementation of `vscode.WorkspaceConfiguration`.
+ *
+ * We cannot use the real implementation, because it would make test read and write developers'
+ * VSCode configuration.
+ *
+ * Instead of implementing the entire class, we only just implement `get` and use `as` keyword
+ * to ignore type checking.
+ */
+class FakeWorkspaceConfiguration {
+  constructor(private readonly value: string) {}
+
+  asVscodeType(): vscode.WorkspaceConfiguration {
+    // We need to go through unknown. Otherwise, TypeScript will not let us type cast.
+    return this as unknown as vscode.WorkspaceConfiguration;
+  }
+
+  get(section: string): string {
+    if (section != 'codeSearch') {
+      throw new Error(`unexpected section: ${section}`);
+    }
+    return this.value;
+  }
+}
+
+describe('CodeSearch: searching for selection', async () => {
+  const textDocument = await vscode.workspace.openTextDocument({
+    content: 'Give people the power to share\nand make the world more open and connected.'});
+  const textEditor = await vscode.window.showTextDocument(textDocument);
+  textEditor.selection = new vscode.Selection(0, 5, 0, 11); // selects 'people'
+
+  it('in public CS', async () => {
+    const config = new FakeWorkspaceConfiguration('public').asVscodeType();
+    // TODO(ttylenda): Consider using a library to create test doubles (here and in other tests).
+    let capturedUri: vscode.Uri|undefined;
+
+    const codeSearch = new CodeSearch(
+        () => config,
+        (uri: vscode.Uri) => {
+          capturedUri = uri;
+        },
+    );
+
+    // TODO(ttylenda): Call the VSCode command instead calling the TS method.
+    codeSearch.searchSelection(textEditor);
+
+    assert.deepStrictEqual(capturedUri,
+        vscode.Uri.parse('https://source.chromium.org/search?q=people'));
+  });
+
+  it('in internal CS', async () => {
+    const config = new FakeWorkspaceConfiguration('internal').asVscodeType();
+    let capturedUri: vscode.Uri|undefined;
+
+    const codeSearch = new CodeSearch(
+        () => config,
+        (uri: vscode.Uri) => {
+          capturedUri = uri;
+        },
+    );
+
+    codeSearch.searchSelection(textEditor);
+
+    assert.deepStrictEqual(capturedUri,
+        vscode.Uri.parse('https://source.corp.google.com/search?q=people'));
+  });
+});
+
+// TODO(ttylenda): Test opening the current file.
