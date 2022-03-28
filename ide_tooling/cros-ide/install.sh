5be86768d32e10834b64bccf8f65a31a32b41575
Eric Lok <lokeric@google.com>
2022-03-28 02:35:04 +0000

    cros-ide: Warn if old node version is used
    Warn if user's chroot is not using v14.* for node
    
    BUG=b:208727066
    TEST=chromite/ide_tooling/cros-ide/install.sh
    
    Change-Id: Icfd0b27a9b3b9216ad49e629ad7baa494557d91a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3555229
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Commit-Queue: Eric Lok <lokeric@google.com>
    Tested-by: Eric Lok <lokeric@google.com>
    Auto-Submit: Eric Lok <lokeric@google.com>

--- ide_tooling/cros-ide/install.sh
+++ ide_tooling/cros-ide/install.sh
@@ -3,7 +3,21 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+terminal_color_clear='\033[0m'
+terminal_color_warning='\033[1;31m'
+min_node_ver=v14
+
+echoWarning() {
+  printf "${terminal_color_warning}%s${terminal_color_clear}\n" "$1"
+}
+
 cd "$(dirname "$0")" || exit 1
+
+if [[ $(node --version) != ${min_node_ver}* ]]; then
+  echoWarning "Node version is too low. Please run\
+  '~/chromiumos/src/scripts/update_chroot' to get node\
+  ${min_node_ver} or higher to avoid unexpected issues."
+fi
 
 npm list typescript || npm ci
 npx ts-node ./src/tools/install.ts "$@"
