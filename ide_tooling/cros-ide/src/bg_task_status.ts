ed90325b90c0530af5d903da7a0a7822b7156d46
Tomasz Tylenda <ttylenda@chromium.org>
2022-03-30 17:27:44 +0900

    cros-ide: add ability to change status bar on errors.
    BUG=b:227115594
    TEST=https://screenshot.googleplex.com/57m2tP8WefjPCMJ.png no errors
    TEST=https://screenshot.googleplex.com/8GdcjKK8r8SHzdn.png with errors
    
    Change-Id: If45bbd8186321c5d0c55d536bbda4c90839859e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3557734
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/bg_task_status.ts
+++ ide_tooling/cros-ide/src/bg_task_status.ts
@@ -5,21 +5,59 @@
 import * as vscode from 'vscode';
 import * as ideUtilities from './ide_utilities';
 
-/**
- * Creates a status bar item showing if the IDE works well or not.
- * It is meant for continuously running background tasks, which should not
- * display popups. Clicking on the status bar item takes the user to
- * the log of background tasks (emerge, and so on).
- */
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): StatusManager {
   vscode.commands.registerCommand('cros-ide.showIdeLog', () => {
     ideUtilities.getLogger().show();
   });
+
   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
   statusBarItem.command = 'cros-ide.showIdeLog';
-  statusBarItem.text = 'CrOS IDE';
-  statusBarItem.tooltip = 'Background Tasks';
   statusBarItem.show();
 
-  // TODO(ttylenda): Change color and text (add icon) when errors occur.
+  return new StatusManager(statusBarItem);
+}
+
+/**
+ * Manages a status bar item showing if the IDE works well or not.
+ * Clicking on the status bar item takes the user to the log of
+ * background tasks (emerge, and so on).
+ *
+ * This functionality is meant for continuously running background tasks,
+ * which should not overuse popups.
+ */
+// TODO(ttylenda): test this class
+export class StatusManager {
+  private unhealthyTasks = new Set<string>();
+
+  constructor(private readonly statusBarItem: vscode.StatusBarItem) {
+    this.refresh();
+  }
+
+  addError(taskId: string) {
+    this.unhealthyTasks.add(taskId);
+    this.refresh();
+  }
+
+  deleteError(taskId: string) {
+    this.unhealthyTasks.delete(taskId);
+    this.refresh();
+  }
+
+  /**
+   * Adjusts appearance of the status bar based presence of unhealthy tasks.
+   *
+   * It is possible to have one more status indicating warning, but we keep
+   * things simple for now.
+   */
+  private refresh() {
+    if (this.unhealthyTasks.size) {
+      this.statusBarItem.text = '$(error) CrOS IDE';
+      this.statusBarItem.tooltip = 'Background Tasks (Errors)';
+      this.statusBarItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+    } else {
+      this.statusBarItem.text = '$(check) CrOS IDE';
+      this.statusBarItem.tooltip = 'Background Tasks (No Problems)';
+      this.statusBarItem.backgroundColor = undefined;
+    }
+  }
 }
