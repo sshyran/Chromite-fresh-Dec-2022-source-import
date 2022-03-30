edc2d6d0085b75b6a1b912a7d4f5ce95e4e921d1
Tomasz Tylenda <ttylenda@chromium.org>
2022-03-30 19:07:01 +0900

    cros-ide: show a pop up when generate_cs_path fails.
    BUG=b:227115594
    TEST=https://screenshot.googleplex.com/98PkbeRkZ33qg7T.png
    
    Change-Id: I74076271b598efd67a6d1b26ce81db0837e4714f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3557736
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/codesearch.ts
+++ ide_tooling/cros-ide/src/codesearch.ts
@@ -1,9 +1,9 @@
 // Copyright 2022 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import * as childProcess from 'child_process';
-import * as ideUtilities from './ide_utilities';
 import * as vscode from 'vscode';
+import * as commonUtil from './common/common_util';
+import * as ideUtilities from './ide_utilities';
 
 export function activate(context: vscode.ExtensionContext) {
   const openFileCmd =
@@ -21,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
 const generateCsPath = '~/chromiumos/chromite/contrib/generate_cs_path';
 const codeSearch = 'codeSearch';
 
-function openCurrentFile(textEditor: vscode.TextEditor) {
+async function openCurrentFile(textEditor: vscode.TextEditor) {
   const fullpath = textEditor.document.fileName;
 
   // Which CodeSearch to use, options are public, internal, or gitiles.
@@ -30,18 +30,25 @@ function openCurrentFile(textEditor: vscode.TextEditor) {
   const line = textEditor.selection.active.line + 1;
 
   // generate_cs_path is a symlink that uses a wrapper to call a Python script,
-  // so it seems we need exec(), which spawn a shell.
-  childProcess.exec(
-      `${generateCsPath} --show "--${csInstance}" --line=${line} "${fullpath}"`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.log(stderr);
-          return;
-        }
-        // trimEnd() to get rid of the newline.
-        vscode.env.openExternal(vscode.Uri.parse(stdout.trimEnd()));
-      },
-  );
+  // so we need to spawn a shell.
+  const res = await commonUtil.exec('sh',
+      ['-c', `${generateCsPath} --show "--${csInstance}" --line=${line} "${fullpath}"`],
+      undefined,
+      {ignoreNonZeroExit: true});
+
+  if (res instanceof Error) {
+    vscode.window.showErrorMessage('Could not run generate_cs_path:' + res);
+    return;
+  }
+
+  const {exitStatus, stdout, stderr} = res;
+  if (exitStatus) {
+    vscode.window.showErrorMessage(`generate_cs_path returned an error: ${stderr}`);
+    return;
+  }
+
+  // trimEnd() to get rid of the newline.
+  vscode.env.openExternal(vscode.Uri.parse(stdout.trimEnd()));
 }
 
 // TODO: Figure out if the search should be limited to the current repo.
