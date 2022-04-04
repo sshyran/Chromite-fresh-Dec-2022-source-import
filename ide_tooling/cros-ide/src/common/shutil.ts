a39dc7c5a2b574c66c3d91ad2c2e67c36b48c921
Tomasz Tylenda <ttylenda@chromium.org>
2022-04-04 13:07:36 +0900

    cros-ide: add utility to escape shell arguments.
    BUG=b:223534220
    TEST=added unit tests
    
    Change-Id: I38e8be1b61ffa7dc15cdcec8b4ead6da3f80cf1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3567847
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>

---
+++ ide_tooling/cros-ide/src/common/shutil.ts
@@ -0,0 +1,31 @@
+// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * safeRE matches an argument that can be literally included in a shell
+ * command line without requiring escaping.
+ *
+ * The character class \w is equivalent to [0-9A-Za-z_]. Leading equals sign is unsafe in zsh,
+ * see http://zsh.sourceforge.net/Doc/Release/Expansion.html#g_t_0060_003d_0027-expansion.
+ */
+const safeRE = /^[-\w@%+:,./][-\w@%+:,./=]*$/;
+
+/**
+ * Escape escapes a string so it can be safely included as an argument in a shell command line.
+ * The string is not modified if it can already be safely included.
+ */
+export function escape(arg: string): string {
+  if (safeRE.test(arg)) {
+    return arg;
+  }
+  return `'` + arg.replace(`'`, `'"'"'`) + `'`;
+}
+
+/**
+ * EscapeArray escapes an array of strings so each will be treated as a separate
+ * argument in the returned shell command line. See Escape for more information.
+ */
+export function escapeArray(args: string[]): string {
+  return args.map(escape).join(' ');
+}
