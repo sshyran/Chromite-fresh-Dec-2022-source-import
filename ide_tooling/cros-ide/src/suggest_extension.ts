be0a987834b988731d71d1c2aac56a36722e02fd
Keigo Oka <oka@google.com>
2022-04-04 07:02:03 +0000

    cros-ide: Remove handler for extension suggestion as appropriate
    We can dispose the subscription for installing an extension when the
    extension is already installed or the user disagrees on installation.
    
    BUG=b:227293635
    TEST=manually
    
    Change-Id: I7419177572af41ab4b7b17372a642f0ae7d51a34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3567851
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

--- ide_tooling/cros-ide/src/suggest_extension.ts
+++ ide_tooling/cros-ide/src/suggest_extension.ts
@@ -5,7 +5,7 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  const suggester = new SuggestExtension([
+  const recommendations: Recommendation[] = [
     {
       languageId: 'cpp',
       languageName: 'C++',
@@ -18,8 +18,11 @@ export function activate(context: vscode.ExtensionContext) {
       extensionId: 'golang.Go',
       extensionName: 'Go',
     },
-  ]);
-  suggester.activate(context);
+  ];
+
+  for (const recommended of recommendations) {
+    activateSingle(context, recommended);
+  }
 }
 
 export interface Recommendation {
@@ -29,47 +32,45 @@ export interface Recommendation {
   extensionName: string,
 }
 
+// TODO(oka): Test this function.
+async function activateSingle(context: vscode.ExtensionContext, recommended: Recommendation) {
+  // Don't install handler if the extension is already installed.
+  if (vscode.extensions.getExtension(recommended.extensionId)) {
+    return;
+  }
+
+  const subscription: vscode.Disposable[] = [];
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(
+      async editor => {
+        if (!editor) {
+          return;
+        }
+        if (editor.document.languageId === recommended.languageId) {
+          if (await suggest(recommended)) {
+            subscription[0].dispose();
+          }
+        }
+      }, undefined, subscription,
+  ));
+}
+
 const YES = 'Yes';
 const LATER = 'Later';
 
-// TODO(oka): Test this class.
-export class SuggestExtension {
-  private readonly ignoredExtensions = new Set();
-
-  constructor(private readonly recommendations: Array<Recommendation>) {
+// Returns true if suggestion is no longer needed.
+async function suggest(recommended: Recommendation): Promise<boolean> {
+  if (vscode.extensions.getExtension(recommended.extensionId)) {
+    return true;
   }
-
-  activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(
-        async editor => {
-          if (!editor) {
-            return;
-          }
-          for (const recommended of this.recommendations) {
-            if (editor.document.languageId === recommended.languageId) {
-              await this.suggest(recommended);
-            }
-          }
-        },
-    ));
+  const message = `It is recommended to install ${recommended.extensionName} extension for ` +
+    `${recommended.languageName}. Proceed?`;
+  const choice = await vscode.window.showInformationMessage(message, YES, LATER);
+  if (choice === YES) {
+    await vscode.commands.executeCommand('extension.open', recommended.extensionId);
+    await vscode.commands.executeCommand('workbench.extensions.installExtension',
+        recommended.extensionId);
+  } else if (choice === LATER) {
+    return true;
   }
-
-  async suggest(recommended: Recommendation) {
-    if (this.ignoredExtensions.has(recommended.extensionId)) {
-      return;
-    }
-    if (vscode.extensions.getExtension(recommended.extensionId)) {
-      return;
-    }
-    const message = `It is recommended to install ${recommended.extensionName} extension for ` +
-      `${recommended.languageName}. Proceed?`;
-    const choice = await vscode.window.showInformationMessage(message, YES, LATER);
-    if (choice === YES) {
-      await vscode.commands.executeCommand('extension.open', recommended.extensionId);
-      await vscode.commands.executeCommand('workbench.extensions.installExtension',
-          recommended.extensionId);
-    } else if (choice === LATER) {
-      this.ignoredExtensions.add(recommended.extensionId);
-    }
-  }
+  return false;
 }
