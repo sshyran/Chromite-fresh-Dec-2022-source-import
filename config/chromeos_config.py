a49bbdc39eb74745a956a25f439a2d1049628810
chromeos-ci-release <chromeos-ci-release@chromeos-bot.iam.gserviceaccount.com>
2022-04-14 09:28:59 -0700

    Update config to include release-R102-14695.B
    Generated by brancher, see https://cr-buildbucket.appspot.com/build/8816893022821944881 for job details.
    
    BUG=None
    TEST=None
    R=chromeos-continuous-integration-team@google.com, engeg@google.com, jackneus@google.com
    
    Change-Id: If32955205b5170ec5e50c2d3997e5efbab9304df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3587110
    Reviewed-by: George Engelbrecht <engeg@google.com>
    Tested-by: George Engelbrecht <engeg@google.com>
    Owners-Override: George Engelbrecht <engeg@google.com>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -2453,6 +2453,14 @@ def BranchScheduleConfig():
   # Do not remove BOT-TAG:* comments. They are used to help parse config.
   # BOT-TAG:RELEASES_START
   RELEASES = [
+      ('release-R102-14695.B',
+       ['kevin-android-pi-pre-flight-branch',
+        'hatch-android-rvc-pre-flight-branch'],
+       '',
+       [],
+       [],
+       config_lib.LUCI_BUILDER_LEGACY_RELEASE),
+
       ('release-R101-14588.B',
        ['kevin-android-pi-pre-flight-branch',
         'hatch-android-rvc-pre-flight-branch'],
@@ -2462,14 +2470,6 @@ def BranchScheduleConfig():
        config_lib.LUCI_BUILDER_LEGACY_RELEASE),
 
       ('release-R100-14526.B',
-       ['kevin-android-pi-pre-flight-branch',
-        'hatch-android-rvc-pre-flight-branch'],
-       '',
-       [],
-       [],
-       config_lib.LUCI_BUILDER_LEGACY_RELEASE),
-
-      ('release-R99-14469.B',
        ['kevin-android-pi-pre-flight-branch',
         'hatch-android-rvc-pre-flight-branch'],
        '',
