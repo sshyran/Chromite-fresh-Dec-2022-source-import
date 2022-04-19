458b64491a30752dca92ba347ec94ea5f362643e
Shuhei Takahashi <nya@chromium.org>
2022-04-19 07:38:37 +0000

    cros-ide: Reformat comments in webpack.config.js
    BUG=b:227709647
    TEST=None
    
    Change-Id: Iacaf76905b2045897ba0238cee70edff18eba414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3592657
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- ide_tooling/cros-ide/webpack.config.js
+++ ide_tooling/cros-ide/webpack.config.js
@@ -12,25 +12,29 @@ const path = require('path');
 
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: 'node', // vscode extensions run in a Node.js-context
-  //-> https://webpack.js.org/configuration/node/
-  mode: 'none', // this leaves the source code as close as possible to the
-  //original (when packaging we set this to 'production')
+  // vscode extensions run in a Node.js-context
+  // -> https://webpack.js.org/configuration/node/
+  target: 'node',
+  // this leaves the source code as close as possible to the
+  // original (when packaging we set this to 'production')
+  mode: 'none',
 
-  entry: './src/extension.ts', // the entry point of this extension,
-  //-> https://webpack.js.org/configuration/entry-context/
+  // the entry point of this extension,
+  // -> https://webpack.js.org/configuration/entry-context/
+  entry: './src/extension.ts',
   output: {
     // the bundle is stored in the 'dist' folder (check package.json),
-    //-> https://webpack.js.org/configuration/output/
+    // -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
   },
   externals: {
-    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly
-    //and must be excluded. Add other modules that cannot be webpack'ed,
+    // the vscode-module is created on-the-fly
+    // and must be excluded. Add other modules that cannot be webpack'ed,
     // -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
+    vscode: 'commonjs vscode',
   },
   resolve: {
     // support reading TypeScript and JavaScript files,
