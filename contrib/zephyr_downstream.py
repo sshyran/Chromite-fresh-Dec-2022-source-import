9d95ff32535b9453d3f94c34e6f62508078c7d37
Fabio Baltieri <fabiobaltieri@google.com>
2022-04-28 11:38:42 +0000

    contrib: zephyr_downstream: add support for limit and stop-at
    Add two flags to the script: limit, to only tag the most recent changes,
    and stop-at, to stop tagging changes when hitting a specific one. These
    may be helpful to upstream a bunch of changes until a problematic one,
    so that they can go through while the others are being worked on.
    
    BUG=None
    TEST=actually used the flags
    
    Change-Id: I931a0c40fbd67ab13309c8e65ffebd8ee8eb1c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3613807
    Reviewed-by: Aaron Massey <aaronmassey@google.com>
    Commit-Queue: Fabio Baltieri <fabiobaltieri@google.com>
    Tested-by: Fabio Baltieri <fabiobaltieri@google.com>
    Reviewed-by: Benjamin Shai <bshai@google.com>

--- contrib/zephyr_downstream.py
+++ contrib/zephyr_downstream.py
@@ -22,6 +22,12 @@ def main(args):
   parser.add_argument('--dry-run',
                       action='store_true',
                       help='Dry run, no updates to Gerrit.')
+  parser.add_argument('--limit',
+                      type=int,
+                      help='How many changes to modify, from the oldest.')
+  parser.add_argument('--stop-at',
+                      type=str,
+                      help='Stop at the specified change number.')
   opts = parser.parse_args(args)
   dry_run = opts.dry_run
 
@@ -32,12 +38,22 @@ def main(args):
                                  raw=True)
   cls_to_downstream.sort(key=lambda patch: patch['number'])
 
+  if opts.limit:
+    cls_to_downstream = cls_to_downstream[:opts.limit]
+
   logging.info('Downstreaming the following CLs:\n%s',
                '\n'.join((patch['number'] for patch in cls_to_downstream)))
 
+  stop_at = opts.stop_at
   # TODO(aaronmassey): Investigate bulk changes from Gerrit lib API instead.
   for i, patch in enumerate(cls_to_downstream):
     change_num = patch['number']
+
+    if stop_at and stop_at == change_num:
+      logging.info('Matched change: %s, stop processing other changes',
+                   change_num)
+      break
+
     logging.info('Downstreaming %s: %d/%d',
                  change_num,
                  i + 1,
