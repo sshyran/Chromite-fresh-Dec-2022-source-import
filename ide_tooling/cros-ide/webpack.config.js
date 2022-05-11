f8760399473e8cbc06c9a861d1bee6594d30e142
Shuhei Takahashi <nya@chromium.org>
2022-05-11 11:34:01 +0000

    cros-ide: Introduce views
    "views" directory will hold TypeScript source code to be loaded in
    WebView. We need to separate them from the extension source code
    because we build them for different targets (web vs. node).
    
    BUG=b:232317657
    TEST=npm run build
    
    Change-Id: Iebcf82ef8481461b1a7595da031ab74f94b44b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3649391
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- ide_tooling/cros-ide/webpack.config.js
+++ ide_tooling/cros-ide/webpack.config.js
@@ -6,40 +6,55 @@
 'use strict';
 
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
-const extensionConfig = {
-  // vscode extensions run in a Node.js-context
-  // -> https://webpack.js.org/configuration/node/
-  target: 'node',
-  // this leaves the source code as close as possible to the
-  // original (when packaging we set this to 'production')
+const commonConfig = {
+  // This leaves the source code as close as possible to the
+  // original (when packaging we set this to "production").
   mode: 'none',
 
-  // the entry point of this extension,
-  // -> https://webpack.js.org/configuration/entry-context/
+  resolve: {
+    // Support reading TypeScript and JavaScript files.
+    // https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js'],
+  },
+
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    // Enable logging required for problem matchers.
+    level: 'log',
+  },
+};
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  ...commonConfig,
+
+  // VSCode extensions run in a Node.js context.
+  // https://webpack.js.org/configuration/node/
+  target: 'node',
+
+  // The entry point of this extension.
+  // https://webpack.js.org/configuration/entry-context/
   entry: './src/extension.ts',
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json),
-    // -> https://webpack.js.org/configuration/output/
+    // The bundle is stored in the dist folder.
+    // package.json specifies dist/extension.js as the extension entry point.
+    // https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
   },
   externals: {
-    // the vscode-module is created on-the-fly
-    // and must be excluded. Add other modules that cannot be webpack'ed,
-    // -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vscodeignore file
+    // The vscode module is created on-the-fly and must be excluded.
+    // Add other modules that cannot be webpack'ed.
+    // https://webpack.js.org/configuration/externals/
+    // Modules added here also need to be added in the .vscodeignore file.
     vscode: 'commonjs vscode',
-  },
-  resolve: {
-    // support reading TypeScript and JavaScript files,
-    // -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
@@ -54,9 +69,48 @@ const extensionConfig = {
       },
     ],
   },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: 'log', // enables logging required for problem matchers
-  },
 };
-module.exports = [extensionConfig];
+
+/** @type WebpackConfig */
+const viewsConfig = {
+  ...commonConfig,
+
+  // WebView scripts run in the web context.
+  target: 'web',
+
+  // Entry points.
+  // https://webpack.js.org/configuration/entry-context/
+  entry: {
+    vnc: './views/src/vnc.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'views'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              // Use tsconfig.json in the subdirectory.
+              configFile: 'views/tsconfig.json',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      // This plugin copies views/static/* to dist/views/*.
+      patterns: [{from: 'views/static', to: '.'}],
+    }),
+  ],
+};
+
+module.exports = [extensionConfig, viewsConfig];
