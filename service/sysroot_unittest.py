0f8cc93e00baeb3dfdabc0e0a6d627513991c908
Manoj Gupta <manojgupta@chromium.org>
2022-06-18 16:13:33 +0000

    Revert "service: Migrate final emerge call to Python"
    This reverts commit 0bde8c5644f616061bc815d5ca704c28ede090f4.
    
    Reason for revert: chrome-icu breakage, b/236411297
    
    BUG=b:236411297
    
    Original change's description:
    > service: Migrate final emerge call to Python
    >
    > Completely migrate build_packages by moving the final call to emerge to
    > Python.
    >
    > BUG=b:218522022
    > TEST=./run_tests service/sysroot_unittest.py
    >
    > Cq-Depend: chromium:3673120
    > Change-Id: I846ba11073835aae04d3bbcde12271aded6f1f5e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3696039
    > Commit-Queue: Cindy Lin <xcl@google.com>
    > Reviewed-by: Ram Chandrasekar <rchandrasekar@google.com>
    > Tested-by: Cindy Lin <xcl@google.com>
    
    Bug: b:218522022
    Change-Id: I2aba3d35a2f92b1f97fad73ba6e2c13e2443d7eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3712618
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Manoj Gupta <manojgupta@chromium.org>
    Commit-Queue: Cindy Lin <xcl@google.com>
    Tested-by: Manoj Gupta <manojgupta@chromium.org>
    Reviewed-by: Cindy Lin <xcl@google.com>
    Auto-Submit: Manoj Gupta <manojgupta@chromium.org>

--- service/sysroot_unittest.py
+++ service/sysroot_unittest.py
@@ -360,6 +360,21 @@ class BuildPackagesRunConfigTest(cros_test_lib.RunCommandTestCase,
                                  cros_test_lib.LoggingTestCase):
   """Tests for the BuildPackagesRunConfig."""
 
+  def testGetBuildPackagesArgs(self):
+    """Test the build_packages args building for non-empty values."""
+    packages = ['cat/pkg', 'cat2/pkg2']
+    instance = sysroot.BuildPackagesRunConfig(
+        usepkg=True,
+        install_debug_symbols=True,
+        packages=packages,
+        dryrun=True)
+
+    args = instance.GetBuildPackagesArgs()
+
+    # Packages included.
+    for package in packages:
+      self.assertIn(package, args)
+
   def testGetBuildPackagesExtraEnv(self):
     """Test the build_packages extra env."""
     # Test the default config.
@@ -552,10 +567,13 @@ class BuildPackagesTest(cros_test_lib.RunCommandTestCase,
     self.build_target_name_mock = self.PatchObject(
         sysroot_lib.Sysroot, 'build_target_name', return_value=self.board)
 
+    self.build_packages = Path(constants.CROSUTILS_DIR) / 'build_packages.sh'
     self.base_command = [
-        Path(constants.CHROMITE_BIN_DIR) / 'parallel_emerge',
-        f'--sysroot={self.sysroot.path}',
-        f'--root={self.sysroot.path}',
+        self.build_packages,
+        '--board',
+        self.board,
+        '--board_root',
+        self.sysroot.path,
     ]
 
     # Prevent the test from switching the cpu governor.
@@ -573,9 +591,6 @@ class BuildPackagesTest(cros_test_lib.RunCommandTestCase,
   def testSuccess(self):
     """Test successful run."""
     config = sysroot.BuildPackagesRunConfig()
-    self.PatchObject(
-        config, 'GetForceLocalBuildPackages', return_value=['test/package1'])
-    sdk_pkgs = ' '.join(sysroot._CRITICAL_SDK_PACKAGES)  # pylint: disable=protected-access
 
     with cros_test_lib.LoggingCapturer() as logs:
       sysroot.BuildPackages(self.target, self.sysroot, config)
@@ -584,19 +599,9 @@ class BuildPackagesTest(cros_test_lib.RunCommandTestCase,
       # so just make sure we're calling the right command and pass the args not
       # handled by the run config.
       self.assertCommandContains(self.base_command)
-      self.assertCommandContains(
-          ['--reinstall-atoms=test/package1', '--usepkg-exclude=test/package1'])
-      self.assertCommandContains(
-          [f'--useoldpkg-atoms={sdk_pkgs}', f'--rebuild-exclude={sdk_pkgs}'])
-
-      # Verify the extra environment variables are passed correctly.
-      self.assertCommandContains([f'PKGDIR={self.sysroot.path}/packages'])
-
-      # Check and the logs contain the expected output.
       self.AssertLogsContain(logs, 'PORTAGE_BINHOST')
       self.AssertLogsContain(logs, 'Rebuilding Portage cache.')
       self.AssertLogsContain(logs, 'Cleaning stale binpkgs.')
-      self.AssertLogsContain(logs, 'Merging board packages now.')
 
   def testEcleanBinpkgs(self):
     """Test that eclean is called with the expected packages."""
@@ -637,9 +642,11 @@ class BuildPackagesTest(cros_test_lib.RunCommandTestCase,
     failed = ['cat/pkg', 'foo/bar']
     cpvs = [package_info.SplitCPV(p, strict=False) for p in failed]
     self.PatchObject(portage_util, 'ParseDieHookStatusFile', return_value=cpvs)
-    config = sysroot.BuildPackagesRunConfig()
 
-    result = cros_build_lib.CommandResult(cmd=self.base_command, returncode=1)
+    config = sysroot.BuildPackagesRunConfig()
+    command = self.base_command + config.GetBuildPackagesArgs()
+
+    result = cros_build_lib.CommandResult(cmd=command, returncode=1)
     error = cros_build_lib.RunCommandError('Error', result)
     self.PatchObject(
         cros_build_lib,
