62b02559ff62de8b8498cd1459a07c98640541d4
Lizzy Presland <zland@google.com>
2022-06-27 17:42:58 +0000

    Revert "ide_tooling: Compdb conversion bug fixes"
    This reverts commit 5c8258a773c155faaa76d34ad2503b2517d4ba31.
    
    Reason for revert: b/237278710
    
    Original change's description:
    > ide_tooling: Compdb conversion bug fixes
    >
    > * Support ARM
    > * Convert platform2 directory to src/platform2
    >
    > BUG=b:235426819, b:235426830
    > TEST=unit test
    >
    > Change-Id: I8a27c4c26e8a520f9c628ec8bf59248d2687ec99
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3707347
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > Commit-Queue: Keigo Oka <oka@chromium.org>
    > Tested-by: Keigo Oka <oka@chromium.org>
    
    BUG=b:237278710
    TEST=./run_tests passes locally after revert
    
    Change-Id: Ic04a305371c1171857aa9081d4809ef6dd901c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3727608
    Tested-by: Lizzy Presland <zland@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Lizzy Presland <zland@google.com>

--- ide_tooling/scripts/compdb_no_chroot.py
+++ ide_tooling/scripts/compdb_no_chroot.py
@@ -14,120 +14,50 @@ import json
 import os
 import re
 import sys
-from typing import Optional
 
 
-class Converter:
-  """Converts compilation database to work outside chroot"""
-  def __init__(self, external_trunk_path: str):
-    self.external_trunk_path = external_trunk_path
-    self.external_chroot_path = os.path.join(external_trunk_path, 'chroot')
+# Generate non-chroot version of the DB with the following
+# changes:
+#
+# 1. translate file and directory paths
+# 2. call clang directly instead of using CrOS wrappers
+# 3. use standard clang target triples
+# 4. remove a few compiler options that might not be available
+#    in the potentially older clang version outside the chroot
+def update_value(value: str, external_trunk_path):
+  external_chroot_path = f'{external_trunk_path}/chroot'
 
-  def convert_filepath(self, filepath: str) -> str:
-    # If out-of-tree build is enabled, source files under /mnt/host/source are
-    # used. This directory is inaccessible outside chroot, so we convert it.
-    m = re.fullmatch(r'(\.\.(/\.\.)*)?/mnt/host/source/(.*)', filepath)
-    if m:
-      return os.path.join(self.external_trunk_path, m[3])
+  value = re.sub(r'(\.\./|\.\.)*/mnt/host/source/', f'{external_trunk_path}/',
+                 value)
+  value = re.sub(r'/build/(\S*)', f'{external_chroot_path}/build/\\1', value)
+  value = re.sub(r'-isystem /', f'-isystem {external_chroot_path}/', value)
+  value = re.sub(r'[a-z0-9_]+-(cros|pc)-linux-gnu([a-z]*)?-clang', 'clang',
+                 value)
+  # TODO(oka): Cover this logic with unit test.
+  for x in [r'\b-fdebug-info-for-profiling\b', r'\b-mretpoline\b',
+            r'\b-mretpoline-external-thunk\b', r'\b-mfentry\b']:
+    value = re.sub(x, '', value)
 
-    # If out-of-tree build is disabled, source files are copied in a temporary
-    # directory, and the filepath may point to the copied file. We convert this
-    # so that code navigation doesn't jump to the file in chroot.
-    # We use a heuristic (using /platform2/ as a marker) here to support
-    # platform2 packages.
-    # TODO(oka): Revisit this logic to support packages outside platform2.
-    m = re.fullmatch(r'.*/platform2(/.*)?', filepath)
-    if m:
-      platform2 = os.path.join(self.external_trunk_path, 'src/platform2')
-      if m[1]:
-        return os.path.join(platform2, m[1][1:])
-      return platform2
-
-    if filepath.startswith('/'):
-      return os.path.join(self.external_chroot_path, filepath[1:])
-
-    return filepath
-
-  def convert_clang_option_value(self, value: str) -> str:
-    if '/' in value:
-      return self.convert_filepath(value)
-    return value
-
-  def convert_clang_option(self, option: str) -> Optional[str]:
-    if not option.startswith('-'):
-      return self.convert_clang_option_value(option)
-
-    # Convert flag value
-    if option.startswith('-I'):
-      return '-I' + self.convert_filepath(option[2:])
-
-    # Remove a few compiler options that might not be available in the
-    # potentially older clang version outside the chroot.
-    # TODO(oka): Cover this logic with unit test.
-    if option in ['-fdebug-info-for-profiling', '-mretpoline',
-                  '-mretpoline-external-thunk', '-mfentry']:
-      return None
-
-    if '=' in option:
-      flag, value = option.split('=', 2)
-      return flag + '=' + self.convert_clang_option_value(value)
-
-    if '/' in option:
-      raise Exception(f'Unknown flag that suffixes a filepath: {option}')
-    return option
-
-  def convert_command(self, command: str) -> str:
-    exe, *options = command.split(' ')
-
-    # We should call clang directly instead of using CrOS wrappers.
-    converted_exe: str
-    for x in ['clang', 'clang++']:
-      if exe.endswith(x):
-        converted_exe = x
-        break
-    if not converted_exe:
-      raise Exception(f'Unexpected executable name: {exe}')
-
-    converted_options = []
-    if re.match(r'^(aarch64|arm)', exe):
-      converted_options.append('--target=arm')
-    for option in options:
-      converted_option = self.convert_clang_option(option)
-      if converted_option:
-        converted_options.append(converted_option)
-
-    # Add "-stdlib=libc++" so that the clang outside the chroot can
-    # find built-in headers like <string> and <memory>
-    converted_options.append('-stdlib=libc++')
-
-    return converted_exe + ' ' + ' '.join(converted_options)
+  return value
 
 def generate(data, external_trunk_path):
-  """Generates non-chroot version of the compilation database"""
-
-  converter = Converter(external_trunk_path)
-
-  converted = []
+  out = []
   for x in data:
-    directory = x['directory']
-    command = x['command']
-    filepath = x['file']
-    output = x['output']
-
-    converted.append({
-        'directory': converter.convert_filepath(directory),
-        'command': converter.convert_command(command),
-        'file': converter.convert_filepath(filepath),
-        'output': converter.convert_filepath(output),
-    })
-  return converted
+    updated = {}
+    for key, value in x.items():
+      updated[key] = update_value(value, external_trunk_path)
+    # Add "-stdlib=libc++" so that the clang outside the chroot can
+    # find built-in headers like <string> and <memory>
+    updated['command'] += ' -stdlib=libc++'
+    out.append(updated)
+  return out
 
 def main():
   data = json.load(sys.stdin)
   external_trunk_path = sys.argv[1]
   if not os.path.exists(external_trunk_path):
     raise Exception(f'{external_trunk_path} should be trunk path')
-  json.dump(generate(data, sys.argv[1]), sys.stdout, indent=2)
+  json.dump(generate(data, sys.argv[1]), sys.stdout)
 
 if __name__ == '__main__':
   main()
