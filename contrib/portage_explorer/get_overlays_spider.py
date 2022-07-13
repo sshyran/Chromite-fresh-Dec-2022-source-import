1734cae2fd73cfd6cac2d4ade63eb703a380bde4
Alexander Liu <alexanderliu@google.com>
2022-07-13 22:25:58 +0000

    contrib: portage_explorer: Spider to get overlays
    Add the spider and its unittest to get all the overlays. The spider
    should append to the final output with all the overlays, its src path,
    and its name.
    
    BUG=b:238346133
    TEST=overlay paths match with "cros_list_overlays --all | sed -En
    's/^.*source\/(.*)$/\1/p | sort" and unittest passes
    
    Change-Id: Icf0e09c14c295c314a320399448309521dfb0f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3761452
    Tested-by: Alexander Liu <alexanderliu@google.com>
    Commit-Queue: Alexander Liu <alexanderliu@google.com>
    Reviewed-by: Alex Klein <saklein@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>

---
+++ contrib/portage_explorer/get_overlays_spider.py
@@ -0,0 +1,28 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Spider to get all the overlays."""
+
+from pathlib import Path
+
+from chromite.contrib.portage_explorer import spiderlib
+from chromite.lib import constants
+from chromite.lib import portage_util
+
+
+def execute(output: spiderlib.SpiderOutput):
+  """Get all overlay source paths and names.
+
+  Get all the overlay paths and names. Parse the overlays for the path starting
+  from src/ and get the overlay name from the path. Add all of these to the
+  overlays list in output argument.
+
+  Args:
+    output: SpiderOutput representing the final output from all the spiders.
+  """
+  overlay_paths = sorted(portage_util.FindOverlays(constants.BOTH_OVERLAYS))
+  for path in overlay_paths:
+    src_path = Path(path).relative_to(constants.SOURCE_ROOT)
+    overlay_name = portage_util.GetOverlayName(path)
+    output.overlays.append(spiderlib.Overlay(src_path, overlay_name))
