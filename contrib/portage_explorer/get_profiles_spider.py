98613acfec704dc918479d892d5c53603cb0a6eb
Alexander Liu <alexanderliu@google.com>
2022-07-20 04:00:19 +0000

    contrib: portage_explorer: Spider to get profiles
    Add the spider to get all the profiles and connect with the boards and
    overlays. This is the next step in filling out the huge protobuf for the
    PortageExplorerService.
    
    BUG=b:239644180
    TEST=unittest passes
    
    Change-Id: I5a1a0284629c857479dcb0a82496060a24e4f9df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3775678
    Commit-Queue: Alexander Liu <alexanderliu@google.com>
    Tested-by: Alexander Liu <alexanderliu@google.com>
    Reviewed-by: Alex Klein <saklein@chromium.org>

---
+++ contrib/portage_explorer/get_profiles_spider.py
@@ -0,0 +1,45 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Spider to get all profiles and connect them to the correct board/overlay."""
+
+from pathlib import Path
+
+from chromite.contrib.portage_explorer import get_boards_spider
+from chromite.contrib.portage_explorer import spiderlib
+from chromite.lib import constants
+
+
+def execute(output: spiderlib.SpiderOutput):
+  """Get all profiles and add to respective overlay and board.
+
+  Read all overlays' profiles folder and record the profile's id
+  (overlay_name:profile_name), src path, and name. Connect the profiles to
+  overlays and boards. Connect a profile to a board's private overlay's base
+  profile if it exists, else the public overlay's base profile.
+
+  Args:
+    output: SpiderOutput representing the final output from all the spiders.
+  """
+  board_profiles = {}
+  for overlay in output.overlays:
+    overlay_profiles_path = (Path(constants.SOURCE_ROOT) / overlay.path /
+                             'profiles')
+    for profile_path in overlay_profiles_path.glob('*/'):
+      if profile_path.is_dir():
+        profile_name = profile_path.name
+        profile_id = f'{overlay.name}:{profile_name}'
+        profile_src_path = overlay.path / 'profiles' / profile_name
+        profile = spiderlib.Profile(profile_id, profile_src_path,
+                                    profile_name)
+        overlay.profiles.append(profile)
+        board_name = get_boards_spider.get_board_name(str(overlay.path))
+        if 'private' in overlay.name and profile_name == 'base':
+          board_profiles[board_name] = profile
+        elif board_name not in board_profiles and profile_name == 'base':
+          board_profiles[board_name] = profile
+      overlay.profiles = sorted(overlay.profiles,
+                                key=lambda profile: profile.name)
+  for build_target in output.build_targets:
+    build_target.profile = board_profiles[build_target.name]
