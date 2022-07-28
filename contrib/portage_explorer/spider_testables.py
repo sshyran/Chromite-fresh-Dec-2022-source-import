decb5e3900112aca91b230daab0bfa2332a3a52e
Alexander Liu <alexanderliu@google.com>
2022-07-28 17:18:48 +0000

    contrib: portage_explorer: Parent profiles testing
    Add functionality for spider testing to work with profile inheritance.
    Return both the before adding parents and after adding parents spider
    profiles to be able to compare the final output in the unittest.
    
    BUG=b:239644180
    TEST=works with unittests
    
    Change-Id: I44eef2df2ebbd3261f647eb5451ea61c98db2de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3791839
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>
    Commit-Queue: Alexander Liu <alexanderliu@google.com>
    Tested-by: Alexander Liu <alexanderliu@google.com>

--- contrib/portage_explorer/spider_testables.py
+++ contrib/portage_explorer/spider_testables.py
@@ -5,7 +5,7 @@
 """Utilities for testing spiders in portage_explorer."""
 
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import chromite as cr
 from chromite.contrib.portage_explorer import spiderlib
@@ -34,8 +34,10 @@ def create_overlays(tmp_path: Path, name: str) -> Tuple[cr.test.Overlay,
 
 def create_profiles(
     tmp_path: Path, test_overlay: cr.test.Overlay,
-    profile_names: List[str]) -> Tuple[Dict[str, cr.test.Profile],
-                                       Dict[str, spiderlib.Profile]]:
+    profile_names: List[str],
+    parent_profiles: Optional[Dict[str, cr.test.Profile]] = None
+    ) -> Tuple[Dict[str, cr.test.Profile], Dict[str, spiderlib.Profile],
+               Dict[str, spiderlib.Profile]]:
   """Create test profiles for unittesting spiders.
 
   Create the unpopulated and populated versions of the profiles for the overlay
@@ -45,18 +47,32 @@ def create_profiles(
     tmp_path: Temporary path to put mock data in
     test_overlay: cr.test.Overlay instance to create a profile within
     profile_names: List of strings representing profile names for this overlay
+    parent_profiles: Dict of profile names matched to corresponding
+        portage_testables profile.
 
   Returns:
     A tuple containing the cr.test.Profiles, the profiles to run through the
     spider, and the profiles to compare the the output against
   """
   test_profiles = {}
+  unpopulated_profiles = {}
   populated_profiles = {}
+  if not parent_profiles:
+    parent_profiles = {}
   for profile_name in profile_names:
-    test_profile = test_overlay.create_profile(Path(profile_name))
-    spider_profile = spiderlib.Profile(
+    test_profile = test_overlay.create_profile(
+        Path(profile_name),
+        profile_parents=parent_profiles.get(profile_name, []))
+    test_profiles[profile_name] = test_profile
+    unpopulated_spider_profile = spiderlib.Profile(
         f'{test_profile.overlay}:{profile_name}',
         test_profile.full_path.relative_to(tmp_path), profile_name)
-    test_profiles[profile_name] = test_profile
-    populated_profiles[profile_name] = spider_profile
-  return (test_profiles, populated_profiles)
+    unpopulated_profiles[profile_name] = unpopulated_spider_profile
+    populated_spider_profile = spiderlib.Profile(
+        f'{test_profile.overlay}:{profile_name}',
+        test_profile.full_path.relative_to(tmp_path), profile_name)
+    for parent in parent_profiles.get(profile_name, []):
+      populated_spider_profile.parent_profiles.append(
+          f'{parent.overlay}:{parent.path}')
+    populated_profiles[profile_name] = populated_spider_profile
+  return (test_profiles, unpopulated_profiles, populated_profiles)
