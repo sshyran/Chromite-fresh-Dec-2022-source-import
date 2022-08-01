7634fca50e4f3ce215d7e66610fdc82e6ac26ebb
Tomasz Tylenda <ttylenda@chromium.org>
2022-08-01 17:56:58 +0900

    cros-ide: fix promise-related lint warnings in gn.ts
    BUG=b:239106925
    TEST=manually by changing indentation and adding empty lines in
    bootstat/BUILD.gn
    
    Change-Id: I24b0411e36de0f53b6b4a9bf79ce2442e95c7df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3802553
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/features/gn.ts
+++ ide_tooling/cros-ide/src/features/gn.ts
@@ -27,47 +27,53 @@ export function activate(
       if (!document.uri.path.includes('src/platform2/')) {
         return;
       }
-      fs.promises.realpath(document.uri.fsPath).then(realpath => {
-        const chroot = commonUtil.findChroot(realpath);
-        if (chroot === undefined) {
-          log.channel.appendLine(
-            'ERROR: chroot not found when attempting `gn format`'
-          );
-          return;
-        }
-        const args = ['format', realpath];
-        commonUtil
-          .exec(path.join(chroot, GN_PATH), args, {
-            logger: log.channel,
-            ignoreNonZeroExit: true,
-            logStdout: true,
-          })
-          .then(res => {
-            if (res instanceof Error) {
-              log.channel.appendLine(
-                'ERROR: failed to run `gn format`: ' + res.message
-              );
-              statusManager.setTask(log.taskId, {
-                status: bgTaskStatus.TaskStatus.ERROR,
-                command: log.showLogCommand,
-              });
-              return;
-            }
-            // Exit status is 1 for all of these cases:
-            // - There was a syntax error in the file. This should be ignored.
-            // - Couldn't read the. This should be reported to the user.
-            // - Other errors (e.g. wrong subcommand name to gn, etc.)
-            // These can be distinguished by the error messages, but it is not a public API.
-            if (
-              res.exitStatus === 1 &&
-              res.stdout.includes("ERROR Couldn't read")
-            ) {
-              log.channel.appendLine(
-                'ERROR: `gn format` command exited with error: ' + res.stdout
-              );
-            }
-          });
-      });
+
+      // Passing an async function to vscode.Event essentially ignores promise
+      // results, so we keep this function non-async and ignore results
+      // explicitly with void.
+      void format(document.uri.fsPath, statusManager, log);
     })
   );
+}
+
+async function format(
+  fsPath: string,
+  statusManager: bgTaskStatus.StatusManager,
+  log: logs.LoggingBundle
+) {
+  const realpath = await fs.promises.realpath(fsPath);
+  const chroot = commonUtil.findChroot(realpath);
+  if (chroot === undefined) {
+    log.channel.appendLine(
+      'ERROR: chroot not found when attempting `gn format`'
+    );
+    return;
+  }
+
+  const args = ['format', realpath];
+  const res = await commonUtil.exec(path.join(chroot, GN_PATH), args, {
+    logger: log.channel,
+    ignoreNonZeroExit: true,
+    logStdout: true,
+  });
+
+  if (res instanceof Error) {
+    log.channel.appendLine('ERROR: failed to run `gn format`: ' + res.message);
+    statusManager.setTask(log.taskId, {
+      status: bgTaskStatus.TaskStatus.ERROR,
+      command: log.showLogCommand,
+    });
+    return;
+  }
+
+  // Exit status is 1 for all of these cases:
+  // - There was a syntax error in the file. This should be ignored.
+  // - Couldn't read the. This should be reported to the user.
+  // - Other errors (e.g. wrong subcommand name to gn, etc.)
+  // These can be distinguished by the error messages, but it is not a public API.
+  if (res.exitStatus === 1 && res.stdout.includes("ERROR Couldn't read")) {
+    log.channel.appendLine(
+      'ERROR: `gn format` command exited with error: ' + res.stdout
+    );
+  }
 }
