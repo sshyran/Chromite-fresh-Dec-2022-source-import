cd4f456ee75ea878bf61b209aa06ba263cf4bf39
Keigo Oka <oka@google.com>
2022-08-04 07:42:47 +0000

    cros-ide: Update license headers
    pre-upload.py skips license header checks for existing files, but still
    we update all the files here because people usually copies the license
    header from an existing file to create a new file.
    
    BUG=b:240887161
    TEST=None
    
    Change-Id: I85b1ed1d5193f0651171b57ce9ed3a6edd4bf9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3808915
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>

--- ide_tooling/cros-ide/.prettierrc.js
+++ ide_tooling/cros-ide/.prettierrc.js
@@ -1,7 +1,7 @@
-// Copyright 2022 The Chromium OS Authors. All rights reserved.
+// Copyright 2022 The ChromiumOS Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 module.exports = {
-  ...require('gts/.prettierrc.json')
-}
+  ...require('gts/.prettierrc.json'),
+};
