f4dbd74f4defa0bf74bd0cdb12c31cf23cf17226
Tim Wawrzynczak <twawrzynczak@chromium.org>
2022-08-11 09:32:32 -0600

    lib: Add ap_firmware_config for draco
    This patch adds support for flashing draco devices with CCD or C2D2.
    
    BUG=none
    TEST=cros ap flash
    
    Change-Id: I6b8b6b0168b6d4c3f6c09dd705a4ac00079ad2ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3826910
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Tested-by: Tarun Tuli <taruntuli@google.com>
    Commit-Queue: Tarun Tuli <taruntuli@google.com>

---
+++ lib/firmware/ap_firmware_config/draco.py
@@ -0,0 +1,69 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Draco configs."""
+
+from chromite.lib.firmware import servo_lib
+
+
+_COMMON_PACKAGES = (
+    'chromeos-ec',
+    'coreboot',
+    'depthcharge',
+    'libpayload',
+    'vboot_reference',
+)
+
+BUILD_WORKON_PACKAGES = _COMMON_PACKAGES + (
+    'coreboot-private-files-baseboard-brya',)
+
+BUILD_PACKAGES = _COMMON_PACKAGES + (
+    'chromeos-bootimage',
+    'coreboot-private-files',
+    'intel-adlfsp',
+)
+
+
+def get_config(servo: servo_lib.Servo) -> servo_lib.ServoConfig:
+  """Get DUT controls and programmer argument to flash Draco.
+
+  Each board needs specific config including the voltage for Vref, to turn
+  on and turn off the SPI flash. get_config() returns servo_lib.ServoConfig
+  with settings to flash a servo for a particular build target.
+  The voltage for this board needs to be set to 3.3 V.
+
+  Args:
+    servo: The servo connected to the target DUT.
+
+  Returns:
+    servo_lib.ServoConfig:
+      dut_control_{on, off}=2d arrays formatted like [["cmd1", "arg1", "arg2"],
+                                                      ["cmd2", "arg3", "arg4"]]
+                            where cmd1 will be run before cmd2.
+      programmer=programmer argument (-p) for flashrom and futility.
+  """
+  dut_control_on = []
+  dut_control_off = []
+
+  # Common flashing sequence for C2D2 and CCD
+  # Shutdown AP so that it enters G3 state.
+  dut_control_on.append(['ec_uart_cmd:apshutdown'])
+  # Sleep to ensure the SoC rails get chance to discharge enough.
+  dut_control_on.append(['sleep:5'])
+  # Block power sequence (PG_PP3300_S5_OD) in order to
+  # prevent leakage to the AP on the SPI pins - b:226438219
+  dut_control_on.append(['ec_uart_cmd:blockseq on'])
+  dut_control_off.append(['ec_uart_cmd:blockseq off'])
+
+  if servo.is_c2d2:
+    programmer = 'raiden_debug_spi:serial=%s' % servo.serial
+  elif servo.is_ccd:
+    dut_control_off.append(['power_state:reset'])
+    programmer = ('raiden_debug_spi:target=AP,custom_rst=True,serial=%s' %
+                  servo.serial)
+  else:
+    raise servo_lib.UnsupportedServoVersionError('%s not supported' %
+                                                 servo.version)
+
+  return servo_lib.ServoConfig(dut_control_on, dut_control_off, programmer)
