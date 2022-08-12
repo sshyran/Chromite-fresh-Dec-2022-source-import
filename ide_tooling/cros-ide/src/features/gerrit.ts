60ecf282796e1e98d5590875cc5730cc568b7b9d
teramon <teramon@google.com>
2022-08-12 14:46:07 +0900

    cros-ide: Show the content of a json file from gerrit API using https API
    BUG=b:216048068
    TEST=Confirmed the message box is shown and the log appears on the DEBUG
    CONSOLE after running the command `Gerrit Demo`.
    
    Change-Id: I758d9bb62d202a3bdbc7dbc0f5fb4c38e3a37344
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3828964
    Commit-Queue: Mio Terakado <teramon@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Tested-by: Mio Terakado <teramon@google.com>

--- ide_tooling/cros-ide/src/features/gerrit.ts
+++ ide_tooling/cros-ide/src/features/gerrit.ts
@@ -3,8 +3,51 @@
 // found in the LICENSE file.
 
 import * as vscode from 'vscode';
+import * as https from 'https';
 
-export function activate(_context: vscode.ExtensionContext) {
-  // TODO: implement the feature
-  void vscode.window.showInformationMessage('Hello gerritIntegration!!');
+export function activate(context: vscode.ExtensionContext) {
+  void vscode.window.showInformationMessage('Hello GerritIntegration!!');
+
+  const demoCmd = vscode.commands.registerCommand(
+    'cros-ide.gerrit',
+    showGerritComments
+  );
+  context.subscriptions.push(demoCmd);
+}
+
+async function showGerritComments() {
+  // TODO(teramon): Construct the URL parsing Git commit
+  const commentsUrl =
+    'https://chromium-review.googlesource.com/changes/Ifbd244655871bbed11f4aa9c18f195502a691704/comments';
+  try {
+    const commentsContent = await httpsGet(commentsUrl);
+    const commentsJson = commentsContent.substring(')]}\n'.length);
+    const contentJson = JSON.parse(commentsJson);
+    void vscode.window.showInformationMessage(contentJson);
+    const debugJson = JSON.stringify(contentJson, null, 2);
+    console.log(debugJson);
+    // TODO(teramon): Show the comments on editor.
+  } catch (err) {
+    void vscode.window.showErrorMessage(
+      `Failed to add Gerrit comments: ${err}`
+    );
+    // TODO(teramon): Avoid showing the error message more than once.
+  }
+}
+
+async function httpsGet(url: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, res => {
+        if (res.statusCode !== 200) {
+          reject(new Error(`status code: ${res.statusCode}`));
+        }
+        const body: Uint8Array[] = [];
+        res.on('data', data => body.push(data));
+        res.on('end', () => {
+          resolve(Buffer.concat(body).toString());
+        });
+      })
+      .on('error', reject);
+  });
 }
