359e86e022fcacffdf7702c0adfc940fa68c36da
teramon <teramon@google.com>
2022-08-17 11:27:10 +0900

    cros-ide: Show Comments extracted from Gerrit API on VScode editor
    BUG=b:216048068
    TEST=Confirmed the comments are shown at the proper characters on the proper line while the file is still a temporary demo.
    
    Change-Id: I3657a22eae0f8fbe0601f26c7647be3134572ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3831827
    Tested-by: Mio Terakado <teramon@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Mio Terakado <teramon@google.com>

--- ide_tooling/cros-ide/src/features/gerrit.ts
+++ ide_tooling/cros-ide/src/features/gerrit.ts
@@ -7,7 +7,6 @@ import * as https from 'https';
 
 export function activate(context: vscode.ExtensionContext) {
   void vscode.window.showInformationMessage('Hello GerritIntegration!!');
-
   const demoCmd = vscode.commands.registerCommand(
     'cros-ide.gerrit',
     showGerritComments
@@ -19,19 +18,25 @@ async function showGerritComments() {
   // TODO(teramon): Construct the URL parsing Git commit
   const commentsUrl =
     'https://chromium-review.googlesource.com/changes/Ifbd244655871bbed11f4aa9c18f195502a691704/comments';
+  const controller = vscode.comments.createCommentController(
+    'comment-sample',
+    'comment-API-sample'
+  );
   try {
     const commentsContent = await httpsGet(commentsUrl);
     const commentsJson = commentsContent.substring(')]}\n'.length);
-    const contentJson = JSON.parse(commentsJson);
-    void vscode.window.showInformationMessage(contentJson);
-    const debugJson = JSON.stringify(contentJson, null, 2);
-    console.log(debugJson);
-    // TODO(teramon): Show the comments on editor.
+    const contentJson = JSON.parse(commentsJson) as ChangeComments;
+    for (const [key, value] of Object.entries(contentJson)) {
+      contentJson[key].forEach(msg => {
+        showCommentInfo(controller, msg);
+      });
+      console.log(value);
+    }
   } catch (err) {
     void vscode.window.showErrorMessage(
       `Failed to add Gerrit comments: ${err}`
+      // TODO(teramon): Avoid showing the error message more than once.
     );
-    // TODO(teramon): Avoid showing the error message more than once.
   }
 }
 
@@ -50,4 +55,50 @@ async function httpsGet(url: string): Promise<string> {
       })
       .on('error', reject);
   });
+}
+
+// Response from Gerrit List Change Comments API.
+// https://gerrit-review.googlesource.com/Documentation/rest-api-changes.html#list-change-comments
+type ChangeComments = {
+  [filepath: string]: CommentInfo[];
+};
+
+type CommentInfo = {
+  author: AccountInfo;
+  range: CommentRange;
+  message: string;
+};
+
+type AccountInfo = {
+  name: string;
+};
+
+type CommentRange = {
+  start_line: number; // 1-based
+  start_character: number; // 0-based
+  end_line: number; // 1-based
+  end_character: number; // 0-based
+};
+
+function showCommentInfo(
+  controller: vscode.CommentController,
+  commentInfo: CommentInfo
+) {
+  const dataRange = new vscode.Range(
+    commentInfo.range.start_line - 1,
+    commentInfo.range.start_character,
+    commentInfo.range.end_line - 1,
+    commentInfo.range.end_character
+  );
+  const newComment: vscode.Comment[] = [
+    {
+      author: {
+        name: commentInfo.author.name,
+      },
+      body: commentInfo.message,
+      mode: vscode.CommentMode.Preview,
+    },
+  ];
+  const dataUri = vscode.window.activeTextEditor?.document.uri;
+  controller.createCommentThread(dataUri!, dataRange, newComment);
 }
