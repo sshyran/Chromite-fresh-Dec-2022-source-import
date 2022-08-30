d691bf1bc9337f41ecb60de51fd6fb774d08a6c5
Alex Klein <saklein@chromium.org>
2022-08-30 13:22:57 -0600

    format: Add python formatter
    Add python formatter using black.
    
    BUG=b:233893248, b:235526476
    TEST=manual
    
    Change-Id: I88d3cbdd51f111945f47dd7a3197a3db388021b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3832133
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Alex Klein <saklein@chromium.org>

--- format/formatters/__init__.py
+++ format/formatters/__init__.py
@@ -7,6 +7,7 @@
 from chromite.format.formatters import cpp
 from chromite.format.formatters import gn
 from chromite.format.formatters import json
+from chromite.format.formatters import python
 from chromite.format.formatters import repo_manifest
 from chromite.format.formatters import rust
 from chromite.format.formatters import whitespace
---
+++ format/formatters/python.py
@@ -0,0 +1,50 @@
+# Copyright 2022 The ChromiumOS Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Python formatter."""
+
+from pathlib import Path
+
+from chromite.lib import constants
+from chromite.lib import cros_build_lib
+
+
+def Data(data: str) -> str:
+  """Format python |data|.
+
+  Args:
+   data: The file content to lint.
+
+  Returns:
+    Formatted data.
+  """
+  # We run through isort first to enforce module sorting order, then run that
+  # result through black. We can't run isort independently (or after black)
+  # because it has some known edge cases where it formats in black-incompatible
+  # ways. See b/235526476 for details.
+  result = cros_build_lib.run(
+      [
+          Path(constants.CHROMITE_SCRIPTS_DIR) / 'isort',
+          '-',
+          '-d',
+      ],
+      capture_output=True,
+      input=data,
+      encoding='utf-8',
+  )
+
+  config_path = Path(constants.CHROMITE_DIR) / 'pyproject.toml'
+  result = cros_build_lib.run(
+      [
+          Path(constants.CHROMITE_SCRIPTS_DIR) / 'black',
+          f'--config={config_path}',
+          '--target-version=py36',
+          '--code',
+          result.stdout,
+      ],
+      capture_output=True,
+      encoding='utf-8',
+  )
+
+  return result.stdout
