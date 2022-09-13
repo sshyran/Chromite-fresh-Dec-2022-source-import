6c3339382707887937152ab200fac4394d88928a
teramon <teramon@google.com>
2022-09-13 18:44:15 +0900

    cros-ide: Parse files on git diff
    BUG=b:216048068
    TEST=Run `npm run unit-test`
    
    Change-Id: If56029b34d30e3983dcac15cbde96124f68ba4c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3892297
    Tested-by: Mio Terakado <teramon@google.com>
    Commit-Queue: Mio Terakado <teramon@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- ide_tooling/cros-ide/src/features/gerrit/git.ts
+++ ide_tooling/cros-ide/src/features/gerrit/git.ts
@@ -2,6 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export type Hunks = {
+  [filePath: string]: Hunk[];
+};
+
 export type Hunk = {
   originalStartLine: number;
   originalLineSize: number;
@@ -12,18 +16,39 @@ export type Hunk = {
 /**
  * Parses the output of `git diff -U0` and returns hunks.
  */
-export function getHunk(gitDiffContent: string): Hunk[] {
-  const gitDiffRegex = /@@ -([0-9]*)[,]?([0-9]*) \+([0-9]*)[,]?([0-9]*) @@/gm;
-  const hunks: Hunk[] = [];
-  let range: RegExpExecArray | null;
-  while ((range = gitDiffRegex.exec(gitDiffContent)) !== null) {
-    const hunk: Hunk = {
-      originalStartLine: Number(range[1]),
-      originalLineSize: Number(range[2]),
-      currentStartLine: Number(range[3]),
-      currentLineSize: Number(range[4]),
-    };
-    hunks.push(hunk);
+export function getHunk(gitDiffContent: string): Hunks {
+  /**
+   * gitDiffContent example:`
+   * --- a/ide_tooling/cros-ide/src/features/gerrit.ts
+   * +++ b/ide_tooling/cros-ide/src/features/gerrit.ts
+   * @@ -1,2 +3,4 @@
+   * @@ -10,11 +12,13@@
+   * --- a/ide_tooling/cros-ide/src/features/git.ts
+   * +++ b/ide_tooling/cros-ide/src/features/git.ts
+   * @@ -1,2 +3,4 @@
+   * `
+   */
+  const gitDiffHunkRegex =
+    /(?:(?:^--- a\/(.*)$)|(?:^@@ -([0-9]*)[,]?([0-9]*) \+([0-9]*)[,]?([0-9]*) @@))/gm;
+  let regexArray: RegExpExecArray | null;
+  const hunksAllFiles: Hunks = {};
+  let hunksEachFile: Hunk[] = [];
+  let hunkFilePath = '';
+  while ((regexArray = gitDiffHunkRegex.exec(gitDiffContent)) !== null) {
+    if (regexArray[1]) {
+      console.log(hunkFilePath + ':\n' + hunksAllFiles[hunkFilePath]);
+      hunkFilePath = regexArray[1];
+      hunksEachFile = [];
+    } else {
+      const hunk: Hunk = {
+        originalStartLine: Number(regexArray[2] || '1'),
+        originalLineSize: Number(regexArray[3]),
+        currentStartLine: Number(regexArray[4] || '1'),
+        currentLineSize: Number(regexArray[5]),
+      };
+      hunksEachFile.push(hunk);
+      hunksAllFiles[hunkFilePath] = hunksEachFile;
+    }
   }
-  return hunks;
+  return hunksAllFiles;
 }
--- ide_tooling/cros-ide/src/test/unit/features/gerrit/gerrit_git.test.ts
+++ ide_tooling/cros-ide/src/test/unit/features/gerrit/gerrit_git.test.ts
@@ -12,46 +12,79 @@ diff --git a/ide_tooling/cros-ide/src/features/gerrit.ts b/ide_tooling/cros-ide/
 index 511bb797b..e475e16d4 100644
 --- a/ide_tooling/cros-ide/src/features/gerrit.ts
 +++ b/ide_tooling/cros-ide/src/features/gerrit.ts
-@@ -12 +12 @@ export function activate(context: vscode.ExtensionContext) {
+@@ -2 +2 @@ export function activate(context: vscode.ExtensionContext) {
 -  void vscode.window.showInformationMessage('Hello GerritIntegration!!');
 +  // void vscode.window.showInformationMessage('Hello GerritIntegration!!');
-@@ -15,0 +16 @@ export function activate(context: vscode.ExtensionContext) {
+@@ -3,1 +4 @@ export function activate(context: vscode.ExtensionContext) {
 +      console.log('active.');
-@@ -19,0 +21,6 @@ export function activate(context: vscode.ExtensionContext) {
-+
+@@ -5,2 +7,3 @@ export function activate(context: vscode.ExtensionContext) {
 +  context.subscriptions.push(
-+    vscode.workspace.onDidChangeTextDocument(document => {
 +      void shiftCommentsOnEdit();
-+    })
 +  );
+diff --git a/ide_tooling/cros-ide/src/features/git.ts b/ide_tooling/cros-ide/src/features/git.ts
+index 511bb797b..e475e16d4 100644
+--- a/ide_tooling/cros-ide/src/features/git.ts
++++ b/ide_tooling/cros-ide/src/features/git.ts
+@@ -3 +3 @@ export function activate(context: vscode.ExtensionContext) {
+-  void vscode.window.showInformationMessage('Hello GerritIntegration!!');
++  // void vscode.window.showInformationMessage('Hello GerritIntegration!!');
+@@ -4,1 +5 @@ export function activate(context: vscode.ExtensionContext) {
++      console.log('active.');
+@@ -6,2 +8,3 @@ export function activate(context: vscode.ExtensionContext) {
++  context.subscriptions.push(
++      void shiftCommentsOnEdit();
++  );
+
 `;
 
 describe('Gerrit support', () => {
   it('handles empty diffs', () => {
-    const hunkrangesEmpty = git.getHunk(testDiffEmpty);
-    expect(hunkrangesEmpty).toEqual([]);
+    const hunkRangesEmpty = git.getHunk(testDiffEmpty);
+    expect(hunkRangesEmpty).toEqual({});
   });
-  it('extracts ranges of each hunt', () => {
-    const hunkranges = git.getHunk(testDiff);
-    expect(hunkranges).toEqual([
-      {
-        originalStartLine: 12,
-        originalLineSize: 0,
-        currentStartLine: 12,
-        currentLineSize: 0,
-      },
-      {
-        originalStartLine: 15,
-        originalLineSize: 0,
-        currentStartLine: 16,
-        currentLineSize: 0,
-      },
-      {
-        originalStartLine: 19,
-        originalLineSize: 0,
-        currentStartLine: 21,
-        currentLineSize: 6,
-      },
-    ]);
+  it('extracts ranges of each hunk', () => {
+    const hunkRanges = git.getHunk(testDiff);
+    expect(hunkRanges).toEqual({
+      'ide_tooling/cros-ide/src/features/gerrit.ts': [
+        {
+          originalStartLine: 2,
+          originalLineSize: 0,
+          currentStartLine: 2,
+          currentLineSize: 0,
+        },
+        {
+          originalStartLine: 3,
+          originalLineSize: 1,
+          currentStartLine: 4,
+          currentLineSize: 0,
+        },
+        {
+          originalStartLine: 5,
+          originalLineSize: 2,
+          currentStartLine: 7,
+          currentLineSize: 3,
+        },
+      ],
+      'ide_tooling/cros-ide/src/features/git.ts': [
+        {
+          originalStartLine: 3,
+          originalLineSize: 0,
+          currentStartLine: 3,
+          currentLineSize: 0,
+        },
+        {
+          originalStartLine: 4,
+          originalLineSize: 1,
+          currentStartLine: 5,
+          currentLineSize: 0,
+        },
+        {
+          originalStartLine: 6,
+          originalLineSize: 2,
+          currentStartLine: 8,
+          currentLineSize: 3,
+        },
+      ],
+    });
   });
 });
