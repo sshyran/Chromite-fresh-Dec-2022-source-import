245b02947f1e2b8d5976c036cd9ebd100dc96db8
Keigo Oka <oka@google.com>
2022-09-30 03:42:35 +0000

    cros-ide: Don't insert header if file is not empty
    BUG=b:249864555
    FIXED=b:249864555
    TEST=npm t
    TEST=manually copied a file and confirmed the header is not inserted.
    TEST=manually created a new file and confirmed the header is inserted.
    
    Change-Id: Idf6ed93bd156c5c0f17d7e0d512c972f6daf92de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3925291
    Tested-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/features/new_file_template.ts
+++ ide_tooling/cros-ide/src/features/new_file_template.ts
@@ -22,7 +22,7 @@ export class NewFileTemplate implements vscode.Disposable {
 
   private async handle(uri: vscode.Uri) {
     const document = await vscode.workspace.openTextDocument(uri);
-    const text = textToInsert(document.languageId);
+    const text = textToInsert(document);
     if (!text) {
       return;
     }
@@ -67,11 +67,15 @@ const SLASH_COMMENT_LANGUAGES = new Set([
 
 const HASH_COMMENT_LANGUAGES = new Set(['gn', 'python', 'shellscript']);
 
-function textToInsert(languageId: string): string | undefined {
-  if (SLASH_COMMENT_LANGUAGES.has(languageId)) {
+function textToInsert(document: vscode.TextDocument): string | undefined {
+  if (document.lineCount > 3) {
+    // The license header may already exist.
+    return undefined;
+  }
+  if (SLASH_COMMENT_LANGUAGES.has(document.languageId)) {
     return fillTemplate(TEMPLATE_SLASH_COMMENT);
   }
-  if (HASH_COMMENT_LANGUAGES.has(languageId)) {
+  if (HASH_COMMENT_LANGUAGES.has(document.languageId)) {
     return fillTemplate(TEMPLATE_HASH_COMMENT);
   }
 }
