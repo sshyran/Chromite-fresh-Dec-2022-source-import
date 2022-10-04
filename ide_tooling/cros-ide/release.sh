2c8ec5e90a0cb8f24237a559997c36a2f9136b0a
Keigo Oka <oka@google.com>
2022-10-04 08:52:50 +0000

    cros-ide: Put developer tool for release in the repository
    This patch adds a script to reduce manual process on our release.
    See go/cros-ide-release for our release process.
    
    BUG=b:246668828
    TEST=manually tested `./release.sh update --pre-release` and `./release.sh
    update`.
    TEST=manually tested `./release.sh publish --pre-release` and
    `./release.sh publish` without actually executing the commands to
    publish.
    
    Change-Id: I853edcf823be90912595a4619808a996d82ccb27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3932420
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>

--- ide_tooling/cros-ide/release.sh
+++ ide_tooling/cros-ide/release.sh
@@ -5,38 +5,7 @@
 
 set -e
 
-if [[ -z "${OVSX_PAT}" ]]; then
-  echo "Set OpenVSX personal access token to OVSX_PAT: read https://github.com/eclipse/openvsx/wiki/Publishing-Extensions " 1>&2
-  exit 1
-fi
-
 cd "$(dirname "$0")"
 
-if [[ -n "$(git status -s)" ]]; then
-  echo "Git status dirty: run the command in clean environment"
-  exit 1
-fi
-
-# Assert HEAD is already merged, i.e. an ancestor of cros/main.
-if ! git merge-base --is-ancestor HEAD cros/main; then
-  echo "HEAD should be an ancestor of cros/main"
-  exit 1
-fi
-
-if ! git diff -U0 -p HEAD~ -- ./package.json | grep '^\+.*"version"' --quiet
-then
-  echo "HEAD commit should update version in package.json"
-  exit 1
-fi
-
-npx vsce package
-
-filename="$(ls "*.vsix")"
-
-echo "Publishing ${filename} to OpenVSX"
-
-npx ovsx publish "${filename}" -p "${OVSX_PAT}"
-
-echo "Done: https://open-vsx.org/extension/Google/cros-ide"
-
-echo "**IMPORTANT** Please manually publish the extension ${filename} to the MS Marketplace https://marketplace.visualstudio.com/manage/publishers/google ."
+OVSX_PAT="${OVSX_PAT:=}" VSCE_PAT="${VSCE_PAT:=}" npx ts-node \
+./src/tools/release.ts "$@"
