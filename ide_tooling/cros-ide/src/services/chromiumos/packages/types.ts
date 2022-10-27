6d6aed8700de06e81d0f55cbf8dd307e6c1a64fe
Keigo Oka <oka@google.com>
2022-10-27 02:48:32 +0000

    cros-ide: Document types in packages properly
    BUG=b:254145837
    TEST=repo upload
    
    Change-Id: Ic6676aa669c43c7644fd722d8efd98a6492726c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/3985010
    Tested-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Tomasz Tylenda <ttylenda@chromium.org>

--- ide_tooling/cros-ide/src/services/chromiumos/packages/types.ts
+++ ide_tooling/cros-ide/src/services/chromiumos/packages/types.ts
@@ -2,8 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export type SourceDir = string; // directory containing source code relative to chromiumos/
-export type Atom = string; // category/packagename e.g. chromeos-base/codelab
+/**
+ * Directory containing source code relative to chromiumos/
+ */
+export type SourceDir = string;
+
+/**
+ * category/packagename e.g. chromeos-base/codelab
+ */
+export type Atom = string;
 
 export interface PackageInfo {
   sourceDir: SourceDir;
