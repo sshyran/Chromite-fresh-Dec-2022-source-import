b250c0c62eb2b0253df4d9f58b77c1a6cc284a0b
Bob Moragues <moragues@chromium.org>
2022-11-02 11:50:50 -0700

    config: Configure factory-trogdor-15210.B builder
    BUG=b:257090539
    TEST=Verify that builds are scheduled and generate results
    
    Change-Id: I7467e324c3f30a58e8374c076c50aba4f8087480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/chromite/+/4000543
    Tested-by: Bob Moragues <moragues@chromium.org>
    Reviewed-by: Jack Neus <jackneus@google.com>
    Commit-Queue: Bob Moragues <moragues@chromium.org>
    Reviewed-by: Madeleine Hardt <hardtmad@google.com>

--- config/chromeos_config.py
+++ config/chromeos_config.py
@@ -1129,6 +1129,7 @@ def FactoryBuilders(site_config, _boards_dict, _ge_build_config):
         (WEEKLY, "factory-puff-13329.B", ["puff", "puff-moblab"]),
         (WEEKLY, "factory-zork-13427.B", ["zork"]),
         (WEEKLY, "factory-trogdor-13443.B", ["trogdor", "strongbad"]),
+        (DAILY, "factory-trogdor-15210.B", ["trogdor", "strongbad"]),
         (DAILY, "factory-strongbad-13963.B", ["trogdor", "strongbad"]),
         (WEEKLY, "factory-volteer-13600.B", ["volteer"]),
         (WEEKLY, "factory-dedede-13683.B", ["dedede", "shotzo"]),
